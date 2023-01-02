4deab0ea49e7524ac44b261e82d90d71ab1d2682
Fatih Enes Selvi <benzer1010@gmail.com>
2023-01-02 17:01:05 +0300

    Service created, Select Operation added

---
+++ Core/ETradeAPI.Application/Abstractions/IProductService.cs
@@ -0,0 +1,14 @@
+﻿using ETradeAPI.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ETradeAPI.Application.Abstractions
+{
+    public interface IProductService
+    {
+        List<Product> GetAll();
+    }
+}
