dfbe730aebf4e9cf1efe2c41d955a1bb308c7e48
Fatih Enes Selvi <benzer1010@gmail.com>
2023-01-03 16:36:43 +0300

    Controller created, Select Operation added

---
+++ Presentation/ETradeAPI.API/Controllers/ProductsController.cs
@@ -0,0 +1,26 @@
+﻿using ETradeAPI.Application.Abstractions;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ETradeAPI.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProductsController : ControllerBase
+    {
+        private readonly IProductService _productService;
+
+        public ProductsController(IProductService productService)
+        {
+            _productService = productService;
+        }
+
+        [HttpGet("getall")]
+        public IActionResult GetAll()
+        {
+            var result = _productService.GetAll();
+
+            return Ok(result);
+        }
+    }
+}
