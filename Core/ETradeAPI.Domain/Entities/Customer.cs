e57191a9c4ae096d95a7b964f95cedafef6fd3ae
Fatih Enes Selvi <benzer1010@gmail.com>
2023-01-04 11:54:56 +0300

    Entity created, Properties added

---
+++ Core/ETradeAPI.Domain/Entities/Customer.cs
@@ -0,0 +1,16 @@
+﻿using ETradeAPI.Domain.Entities.Common;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ETradeAPI.Domain.Entities
+{
+    public class Customer : BaseEntity
+    {
+        public string Name { get; set; }
+
+        public ICollection<Order> Orders { get; set; }
+    }
+}
