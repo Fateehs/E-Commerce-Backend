52ca39265593364c04c22161bca4c28fae92786e
Fatih Enes Selvi <benzer1010@gmail.com>
2023-01-04 11:55:12 +0300

    Entity modified, Property added

--- Core/ETradeAPI.Domain/Entities/Product.cs
+++ Core/ETradeAPI.Domain/Entities/Product.cs
@@ -12,5 +12,7 @@ namespace ETradeAPI.Domain.Entities
         public string Name { get; set; }
         public int Stock { get; set; }
         public long Price { get; set; }
+
+        public ICollection<Order> Orders { get; set; }
     }
 }
