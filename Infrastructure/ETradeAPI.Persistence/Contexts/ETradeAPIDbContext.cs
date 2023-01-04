263187b2df5ce6eda165cdc04d8a28872a10e7f7
Fatih Enes Selvi <benzer1010@gmail.com>
2023-01-04 11:55:35 +0300

    Db Context created, Entities linked

---
+++ Infrastructure/ETradeAPI.Persistence/Contexts/ETradeAPIDbContext.cs
@@ -0,0 +1,19 @@
+﻿using ETradeAPI.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ETradeAPI.Persistence.Contexts
+{
+    public class ETradeAPIDbContext : DbContext
+    {
+        public ETradeAPIDbContext(DbContextOptions options) : base(options) { }
+
+        public DbSet<Product> Products { get; set; }
+        public DbSet<Order> Orders { get; set; }
+        public DbSet<Customer> Customers { get; set; }
+    }
+}
