0a2339d1aaf1a5fa049fa1691aa72a77332f72c5
Fatih Enes Selvi <benzer1010@gmail.com>
2023-01-05 17:50:17 +0300

    Abstract of Repository created, Operations added

---
+++ Core/ETradeAPI.Application/Repositories/IRepository.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace ETradeAPI.Application.Repositories
+{
+    public interface IRepository<T> where T : class
+    {
+        DbSet<T> Table { get; }
+    }
+}
---
+++ Core/ETradeAPI.Application/Repositories/IWriteRepository.cs
@@ -0,0 +1,11 @@
+﻿namespace ETradeAPI.Application.Repositories
+{
+    public interface IWriteRepository<T> : IRepository<T> where T : class
+    {
+        Task<bool> AddAsync(T entity);
+        Task<bool> AddAsync(List<T> entity);
+        Task<bool> Remove(T entity);
+        Task<bool> Remove(string id);
+        Task<bool> UpdateAsync(T entity);
+    }
+}
