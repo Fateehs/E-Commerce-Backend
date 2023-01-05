c7d6bdd84b1d8a14f3577ba938f3e867c1e2aeaf
Fatih Enes Selvi <benzer1010@gmail.com>
2023-01-05 17:53:07 +0300

    Configuration Static Class created, Connection String added

---
+++ Infrastructure/ETradeAPI.Persistence/Configuration.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.Extensions.Configuration;
+
+namespace ETradeAPI.Persistence
+{
+    static class Configuration
+    {
+        static public string ConnectionString
+        {
+            get
+            {
+                ConfigurationManager configurationManager = new();
+                configurationManager.SetBasePath(Path.Combine(Directory.GetCurrentDirectory(), "../../Presentation/ETradeAPI.API"));
+                configurationManager.AddJsonFile("appsettings.json");
+
+                return configurationManager.GetConnectionString("PostgreSQL");
+            }
+        }
+    }
+}
