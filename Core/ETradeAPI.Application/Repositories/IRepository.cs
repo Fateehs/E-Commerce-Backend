b064daa970e0fdcc03a170c73030f6d042565b70
Fatih Enes Selvi <benzer1010@gmail.com>
2023-01-06 14:40:01 +0300

    Files moved

--- Core/ETradeAPI.Application/Repositories/IRepository.cs
+++
@@ -1,9 +0,0 @@
-﻿using Microsoft.EntityFrameworkCore;
-
-namespace ETradeAPI.Application.Repositories
-{
-    public interface IRepository<T> where T : class
-    {
-        DbSet<T> Table { get; }
-    }
-}
