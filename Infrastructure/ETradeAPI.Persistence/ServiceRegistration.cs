ce8f0ffa37a564076f9432bd495864be710c9779
Fatih Enes Selvi <benzer1010@gmail.com>
2023-01-06 14:42:31 +0300

    Repositories added to IoC Container

--- Infrastructure/ETradeAPI.Persistence/ServiceRegistration.cs
+++ Infrastructure/ETradeAPI.Persistence/ServiceRegistration.cs
@@ -1,14 +1,8 @@
-﻿using ETradeAPI.Application.Abstractions;
-using ETradeAPI.Persistence.Concretes;
+﻿using ETradeAPI.Application.Repositories;
 using ETradeAPI.Persistence.Contexts;
+using ETradeAPI.Persistence.Repositories;
 using Microsoft.EntityFrameworkCore;
-using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace ETradeAPI.Persistence
 {
@@ -16,8 +10,13 @@ namespace ETradeAPI.Persistence
     {
         public static void AddPersistenceServices(this IServiceCollection services)
         {
-            services.AddDbContext<ETradeAPIDbContext>
-                (options => options.UseNpgsql(Configuration.ConnectionString));
+            services.AddDbContext<ETradeAPIDbContext>(options => options.UseNpgsql(Configuration.ConnectionString), ServiceLifetime.Singleton);
+            services.AddSingleton<ICustomerReadRepository, CustomerReadRepository>();
+            services.AddSingleton<ICustomerWriteRepository, CustomerWriteRepository>();
+            services.AddSingleton<IOrderReadRepository, OrderReadRepository>();
+            services.AddSingleton<IOrderWriteRepository, OrderWriteRepository>();
+            services.AddSingleton<IProductReadRepository, ProductReadRepository>();
+            services.AddSingleton<IProductWriteRepository, ProductWriteRepository>();
         }
     }
 }
