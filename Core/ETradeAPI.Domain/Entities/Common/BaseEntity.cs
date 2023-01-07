f7429a76d667e02121e3cfb388e25858d4d9c3ab
Fatih Enes Selvi <benzer1010@gmail.com>
2023-01-07 16:20:03 +0300

    Base Entity modified, Property added

--- Core/ETradeAPI.Domain/Entities/Common/BaseEntity.cs
+++ Core/ETradeAPI.Domain/Entities/Common/BaseEntity.cs
@@ -10,5 +10,6 @@ namespace ETradeAPI.Domain.Entities.Common
     {
         public Guid Id { get; set; }
         public DateTime CreatedDate { get; set; }
+        public DateTime UpdatedDate { get; set;}
     }
 }
