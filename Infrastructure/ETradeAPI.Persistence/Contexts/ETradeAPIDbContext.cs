4b03627d9afe8deac2fc83b24f53eb0645a02842
Fatih Enes Selvi <benzer1010@gmail.com>
2023-01-07 16:28:43 +0300

    Db Context modified, Interceptor Method added

--- Infrastructure/ETradeAPI.Persistence/Contexts/ETradeAPIDbContext.cs
+++ Infrastructure/ETradeAPI.Persistence/Contexts/ETradeAPIDbContext.cs
@@ -1,4 +1,5 @@
 ﻿using ETradeAPI.Domain.Entities;
+using ETradeAPI.Domain.Entities.Common;
 using Microsoft.EntityFrameworkCore;
 using System;
 using System.Collections.Generic;
@@ -15,5 +16,20 @@ namespace ETradeAPI.Persistence.Contexts
         public DbSet<Product> Products { get; set; }
         public DbSet<Order> Orders { get; set; }
         public DbSet<Customer> Customers { get; set; }
+
+        public override async Task<int> SaveChangesAsync(CancellationToken cancellationToken = default)
+        {
+            var datas = ChangeTracker.Entries<BaseEntity>();
+
+            foreach (var data in datas)
+            {
+                _ = data.State switch
+                {
+                    EntityState.Added => data.Entity.CreatedDate = DateTime.UtcNow,
+                    EntityState.Modified => data.Entity.UpdatedDate = DateTime.UtcNow
+                };
+            }
+            return await base.SaveChangesAsync(cancellationToken);
+        }
     }
 }
