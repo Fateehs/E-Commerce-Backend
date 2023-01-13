34cb4ecc9caeb6a017ae6b73a99bff625f6c2979
Fatih Enes Selvi <benzer1010@gmail.com>
2023-01-13 18:05:45 +0300

    Unnecessary things removed

--- Core/ETradeAPI.Domain/Entities/Common/BaseEntity.cs
+++ Core/ETradeAPI.Domain/Entities/Common/BaseEntity.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace ETradeAPI.Domain.Entities.Common
+﻿namespace ETradeAPI.Domain.Entities.Common
 {
     public class BaseEntity
     {
