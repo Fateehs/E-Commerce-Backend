e815107bb276e9eb3d9cce36bf86991c7da2a642
Fatih Enes Selvi <benzer1010@gmail.com>
2023-01-13 18:05:33 +0300

    Unnecessary things removed

--- Core/ETradeAPI.Application/ViewModels/Products/VM_Create_Product.cs
+++ Core/ETradeAPI.Application/ViewModels/Products/VM_Create_Product.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace ETradeAPI.Application.ViewModels.Products
+﻿namespace ETradeAPI.Application.ViewModels.Products
 {
     public class VM_Create_Product
     {
