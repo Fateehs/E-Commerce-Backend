ddd53ba84029ae72e98663a58613e4c4fa199bbe
Fatih Enes Selvi <benzer1010@gmail.com>
2023-02-13 20:31:47 +0300

    Class created, Methods added

---
+++ Infrastructure/ETradeAPI.Infrastructure/Services/FileService.cs
@@ -0,0 +1,55 @@
+﻿using ETradeAPI.Application.Services;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+
+namespace ETradeAPI.Infrastructure.Services
+{
+    public class FileService : IFileService
+    {
+        readonly IWebHostEnvironment _webHostEnvironment;
+
+        public FileService(IWebHostEnvironment webHostEnvironment)
+        {
+            _webHostEnvironment = webHostEnvironment;
+        }
+
+        public async Task<bool> CopyFileAsync(string path, IFormFile file)
+        {
+            try
+            {
+                await using FileStream fileStream = new
+                    (path, FileMode.Create, FileAccess.Write, FileShare.None, 1024 * 1024, useAsync: false);
+
+                await fileStream.CopyToAsync(fileStream);
+                await fileStream.FlushAsync();
+
+                return true;
+            }
+            catch (Exception ex)
+            {
+
+                throw ex;
+            }
+        }
+
+        public Task<string> FileRenameAsync(string fileName)
+        {
+            throw new NotImplementedException();
+        }
+
+        public async Task<(string fileName, string path)> UploadAsync(string path, IFormFileCollection files)
+        {
+            string uploadPath = Path.Combine(_webHostEnvironment.WebRootPath, path);
+
+            if (!Directory.Exists(uploadPath))
+                Directory.CreateDirectory(uploadPath);
+
+            foreach (IFormFile file in files)
+            {
+                string fileNewName = await FileRenameAsync(file.FileName);
+
+                bool result = await CopyFileAsync(Path.Combine(uploadPath, fileNewName), file);
+            }
+        }
+    }
+}
