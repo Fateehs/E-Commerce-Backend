ff19c4d454bdd2ce561f07f063b41edc0dbbc11e
Fatih Enes Selvi <benzer1010@gmail.com>
2023-02-13 20:31:35 +0300

    Service created, Methods added

---
+++ Core/ETradeAPI.Application/Services/IFileService.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Http;
+
+namespace ETradeAPI.Application.Services
+{
+    public interface IFileService
+    {
+        Task<List<(string fileName, string path)>> UploadAsync(string path, IFormFileCollection files);
+        Task<string> FileRenameAsync(string fileName);
+        Task<bool> CopyFileAsync(string path, IFormFile file);
+    }
+}
