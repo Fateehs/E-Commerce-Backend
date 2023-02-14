bf57dd67acc93ed90a9e76366bfca90c9831c3d9
Fatih Enes Selvi <benzer1010@gmail.com>
2023-02-14 17:26:22 +0300

    Services added

--- Presentation/ETradeAPI.API/Program.cs
+++ Presentation/ETradeAPI.API/Program.cs
@@ -1,4 +1,5 @@
 using ETradeAPI.Application.Validators.Products;
+using ETradeAPI.Infrastructure;
 using ETradeAPI.Infrastructure.Filters;
 using ETradeAPI.Persistence;
 using FluentValidation.AspNetCore;
@@ -6,6 +7,7 @@ using FluentValidation.AspNetCore;
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddPersistenceServices();
+builder.Services.AddInfrastructureServices();
 builder.Services.AddCors(options => options.AddDefaultPolicy(policy =>
 policy.WithOrigins("http://localhost:4200", "https://localhost:4200").AllowAnyHeader().AllowAnyMethod()
 ));
