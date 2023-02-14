eed166be17acefd005006ff6dd45e12e02dcc1fc
Fatih Enes Selvi <benzer1010@gmail.com>
2023-02-14 17:25:00 +0300

    Service Registration Class created

---
+++ Infrastructure/ETradeAPI.Infrastructure/ServiceRegistration.cs
@@ -0,0 +1,21 @@
+﻿using ETradeAPI.Application.Repositories;
+using ETradeAPI.Application.Services;
+using ETradeAPI.Infrastructure.Services;
+using ETradeAPI.Persistence.Contexts;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ETradeAPI.Infrastructure
+{
+    public static class ServiceRegistration
+    {
+        public static void AddInfrastructureServices(this IServiceCollection services)
+        {
+            services.AddScoped<IFileService, FileService>();
+        }
+    }
+}
