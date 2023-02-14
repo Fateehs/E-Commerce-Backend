b16a034342921e32103d8d42bdd9f0cb3887ca68
Fatih Enes Selvi <benzer1010@gmail.com>
2023-02-14 17:24:20 +0300

    Concrete of Service created, Methods added

--- Infrastructure/ETradeAPI.Infrastructure/Services/FileService.cs
+++ Infrastructure/ETradeAPI.Infrastructure/Services/FileService.cs
@@ -1,4 +1,5 @@
 ﻿using ETradeAPI.Application.Services;
+using ETradeAPI.Infrastructure.StaticServices;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
 
@@ -7,7 +8,6 @@ namespace ETradeAPI.Infrastructure.Services
     public class FileService : IFileService
     {
         readonly IWebHostEnvironment _webHostEnvironment;
-
         public FileService(IWebHostEnvironment webHostEnvironment)
         {
             _webHostEnvironment = webHostEnvironment;
@@ -17,39 +17,97 @@ namespace ETradeAPI.Infrastructure.Services
         {
             try
             {
-                await using FileStream fileStream = new
-                    (path, FileMode.Create, FileAccess.Write, FileShare.None, 1024 * 1024, useAsync: false);
+                await using FileStream fileStream = new(path, FileMode.Create, FileAccess.Write, FileShare.None, 1024 * 1024, useAsync: false);
 
-                await fileStream.CopyToAsync(fileStream);
+                await file.CopyToAsync(fileStream);
                 await fileStream.FlushAsync();
-
                 return true;
             }
             catch (Exception ex)
             {
-
                 throw ex;
             }
         }
 
-        public Task<string> FileRenameAsync(string fileName)
+        async Task<string> FileRenameAsync(string path, string fileName, bool first = true)
         {
-            throw new NotImplementedException();
+            string newFileName = await Task.Run<string>(async () =>
+            {
+                string extension = Path.GetExtension(fileName);
+                string newFileName = string.Empty;
+                if (first)
+                {
+                    string oldName = Path.GetFileNameWithoutExtension(fileName);
+                    newFileName = $"{NameOperation.CharacterRegulatory(oldName)}{extension}";
+                }
+                else
+                {
+                    newFileName = fileName;
+                    int indexNo1 = newFileName.IndexOf("-");
+                    if (indexNo1 == -1)
+                        newFileName = $"{Path.GetFileNameWithoutExtension(newFileName)}-2{extension}";
+                    else
+                    {
+                        int lastIndex = 0;
+                        while (true)
+                        {
+                            lastIndex = indexNo1;
+                            indexNo1 = newFileName.IndexOf("-", indexNo1 + 1);
+                            if (indexNo1 == -1)
+                            {
+                                indexNo1 = lastIndex;
+                                break;
+                            }
+                        }
+
+                        int indexNo2 = newFileName.IndexOf(".");
+                        string fileNo = newFileName.Substring(indexNo1 + 1, indexNo2 - indexNo1 - 1);
+
+                        if (int.TryParse(fileNo, out int _fileNo))
+                        {
+                            _fileNo++;
+                            newFileName = newFileName.Remove(indexNo1 + 1, indexNo2 - indexNo1 - 1)
+                                                .Insert(indexNo1 + 1, _fileNo.ToString());
+                        }
+                        else
+                            newFileName = $"{Path.GetFileNameWithoutExtension(newFileName)}-2{extension}";
+                    }
+                }
+
+                if (File.Exists($"{path}\\{newFileName}"))
+                    return await FileRenameAsync(path, newFileName, false);
+                else
+                    return newFileName;
+            });
+
+            return newFileName;
         }
 
-        public async Task<(string fileName, string path)> UploadAsync(string path, IFormFileCollection files)
+        public async Task<List<(string fileName, string path)>> UploadAsync(string path, IFormFileCollection files)
         {
             string uploadPath = Path.Combine(_webHostEnvironment.WebRootPath, path);
 
             if (!Directory.Exists(uploadPath))
                 Directory.CreateDirectory(uploadPath);
 
+            List<(string fileName, string path)> datas = new();
+            List<bool> results = new();
+
             foreach (IFormFile file in files)
             {
-                string fileNewName = await FileRenameAsync(file.FileName);
+                string fileNewName = await FileRenameAsync(uploadPath, file.FileName);
 
                 bool result = await CopyFileAsync(Path.Combine(uploadPath, fileNewName), file);
+
+                datas.Add((fileNewName, Path.Combine(uploadPath, fileNewName)));
+
+                results.Add(result);
             }
+
+            if (results.TrueForAll(r => r.Equals(true)))
+                return datas;
+
+            return null;
         }
     }
 }
