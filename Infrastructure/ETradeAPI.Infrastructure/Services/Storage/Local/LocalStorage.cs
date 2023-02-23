f6a88832fb69c3117d923b0f40a7995894b090c6
Fateeh <benzer1010@gmail.com>
2023-02-23 21:57:18 +0300

    Concrete of Storage Service created, Methods added

---
+++ Infrastructure/ETradeAPI.Infrastructure/Services/Storage/Local/LocalStorage.cs
@@ -0,0 +1,70 @@
+﻿using ETradeAPI.Application.Abstractions.Storage.Local;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ETradeAPI.Infrastructure.Services.Storage.Local
+{
+    public class LocalStorage : ILocalStorage
+    {
+        readonly IWebHostEnvironment _webHostEnvironment;
+        public LocalStorage(IWebHostEnvironment webHostEnvironment)
+        {
+            _webHostEnvironment = webHostEnvironment;
+        }
+
+        public async Task DeleteAsync(string path, string fileName)
+            => File.Delete($"{path}\\{fileName}");
+      
+
+        public List<string> GetFiles(string path)
+        {
+            DirectoryInfo directory = new(path);
+            return directory.GetFiles().Select(f => f.Name).ToList();
+        }
+
+        public bool HasFile(string path, string fileName)
+             => File.Exists($"{path}\\{fileName}");
+
+        private async Task<bool> CopyFileAsync(string path, IFormFile file)
+        {
+            try
+            {
+                await using FileStream fileStream = new(path, FileMode.Create, FileAccess.Write, FileShare.None, 1024 * 1024, useAsync: false);
+
+                await file.CopyToAsync(fileStream);
+                await fileStream.FlushAsync();
+                return true;
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+        }
+
+        public async Task<List<(string fileName, string path)>> UploadAsync(string path, IFormFileCollection files)
+        {
+            string uploadPath = Path.Combine(_webHostEnvironment.WebRootPath, path);
+
+            if (!Directory.Exists(uploadPath))
+                Directory.CreateDirectory(uploadPath);
+
+            List<(string fileName, string path)> datas = new();
+            List<bool> results = new();
+
+            foreach (IFormFile file in files)
+            {
+                bool result = await CopyFileAsync(Path.Combine(uploadPath, file.FileName), file);
+
+                datas.Add((file.FileName, Path.Combine(uploadPath, file.FileName)));
+            }
+
+            return null;
+        }
+    }
+}
---
+++ Infrastructure/ETradeAPI.Infrastructure/Services/Storage/StorageService.cs
@@ -0,0 +1,33 @@
+﻿using ETradeAPI.Application.Abstractions;
+using ETradeAPI.Application.Abstractions.Storage;
+using Microsoft.AspNetCore.Http;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ETradeAPI.Infrastructure.Services.Storage
+{
+    public class StorageService : IStorageService
+    {
+        readonly IStorage _storage;
+
+        public StorageService(IStorage storage)
+        {
+            _storage = storage;
+        }
+
+        public async Task DeleteAsync(string pathOrContainerName, string fileName)
+            => await _storage.DeleteAsync(pathOrContainerName, fileName);
+
+        public List<string> GetFiles(string pathOrContainerName)
+            => _storage.GetFiles(pathOrContainerName);
+
+        public bool HasFile(string pathOrContainerName, string fileName)
+            => _storage.HasFile(pathOrContainerName,fileName);
+
+        public Task<List<(string fileName, string path)>> UploadAsync(string pathOrContainerName, IFormFileCollection files)
+            => _storage.UploadAsync(pathOrContainerName, files);
+    }
+}
