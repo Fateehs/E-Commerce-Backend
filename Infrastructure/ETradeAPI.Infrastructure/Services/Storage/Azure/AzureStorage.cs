756c9bf08e8ccfda07374996269bffa0ab789d02
Fateeh <benzer1010@gmail.com>
2023-02-24 21:27:48 +0300

    Concrete of Azure Storage Class created, Methods implemented

---
+++ Infrastructure/ETradeAPI.Infrastructure/Services/Storage/Azure/AzureStorage.cs
@@ -0,0 +1,53 @@
+﻿using Azure.Storage.Blobs;
+using Azure.Storage.Blobs.Models;
+using ETradeAPI.Application.Abstractions;
+using ETradeAPI.Application.Abstractions.Storage.Azure;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Configuration;
+
+namespace ETradeAPI.Infrastructure.Services.Storage.Azure
+{
+    public class AzureStorage : IAzureStorage
+    {
+        readonly BlobServiceClient _blobServiceClient;
+        BlobContainerClient _blobContainerClient;
+
+        public AzureStorage(IConfiguration configuration)
+        {
+            _blobServiceClient = new(configuration["Storage:Azure"]);
+        }
+
+        public async Task DeleteAsync(string containerName, string fileName)
+        {
+            _blobContainerClient = _blobServiceClient.GetBlobContainerClient(containerName);
+            BlobClient blobClient = _blobContainerClient.GetBlobClient(fileName);
+            await blobClient.DeleteAsync();
+        }
+
+        public List<string> GetFiles(string containerName)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool HasFile(string containerName, string fileName)
+        {
+            throw new NotImplementedException();
+        }
+
+        public async Task<List<(string fileName, string pathOrContainerName)>> UploadAsync(string containerName, IFormFileCollection files)
+        {
+            _blobContainerClient = _blobServiceClient.GetBlobContainerClient(containerName);
+            await _blobContainerClient.CreateIfNotExistsAsync();
+            await _blobContainerClient.SetAccessPolicyAsync(PublicAccessType.BlobContainer);
+
+            List<(string fileName, string pathOrContainerName)> datas = new();
+            foreach (IFormFile file in files)
+            {
+                BlobClient blobClient = _blobContainerClient.GetBlobClient(file.Name);
+                await blobClient.UploadAsync(file.OpenReadStream());
+                datas.Add((file.Name, containerName));
+            }
+            return datas;
+        }
+    }
+}
