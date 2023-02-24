cf41950e79cfdb153ad5f4a1e43b085f36d0543b
Fateeh <benzer1010@gmail.com>
2023-02-24 21:26:40 +0300

    Interace modified, Property modified

--- Core/ETradeAPI.Application/Abstractions/IStorage.cs
+++ Core/ETradeAPI.Application/Abstractions/IStorage.cs
@@ -9,7 +9,7 @@ namespace ETradeAPI.Application.Abstractions
 {
     public interface IStorage
     {
-        Task<List<(string fileName, string path)>> UploadAsync(string pathOrContainerName, IFormFileCollection files);
+        Task<List<(string fileName, string pathOrContainerName)>> UploadAsync(string pathOrContainerName, IFormFileCollection files);
         Task DeleteAsync(string pathOrContainerName, string fileName);
         List<string> GetFiles(string pathOrContainerName);
         bool HasFile(string pathOrContainerName, string fileName);
