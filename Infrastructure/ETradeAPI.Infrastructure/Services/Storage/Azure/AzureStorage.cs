d6512b73a86583138700e22409435ca8c3cd411e
Fateeh <benzer1010@gmail.com>
2023-02-27 19:08:05 +0300

    Storage Class modified

--- Infrastructure/ETradeAPI.Infrastructure/Services/Storage/Azure/AzureStorage.cs
+++ Infrastructure/ETradeAPI.Infrastructure/Services/Storage/Azure/AzureStorage.cs
@@ -6,6 +6,7 @@ using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.Configuration;
 
 namespace ETradeAPI.Infrastructure.Services.Storage.Azure
+{
     public class AzureStorage : Storage, IAzureStorage
     {
         readonly BlobServiceClient _blobServiceClient;
