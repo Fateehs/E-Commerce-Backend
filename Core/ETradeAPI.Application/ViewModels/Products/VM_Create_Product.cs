711f8b6d2c2c5ee72050c507b620594daff3cbe3
Fateeh <benzer1010@gmail.com>
2023-03-01 19:06:01 +0300

    Unused VM removed

--- Core/ETradeAPI.Application/ViewModels/Products/VM_Create_Product.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace ETradeAPI.Application.ViewModels.Products
-{
-    public class VM_Create_Product
-    {
-        public string Name { get; set; }
-        public int Stock { get; set; }
-        public float Price { get; set; }
-    }
-}
