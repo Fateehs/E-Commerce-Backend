141f5f1a408603cf170f548cf852197b82b296e9
Fateeh <benzer1010@gmail.com>
2023-03-01 19:06:54 +0300

    Service Registration Class created

---
+++ Core/ETradeAPI.Application/ServiceRegistration.cs
@@ -0,0 +1,13 @@
+﻿using MediatR;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace ETradeAPI.Application
+{
+    public static class ServiceRegistration
+    {
+        public static void AddApplicationServices(this IServiceCollection services) 
+        {
+            services.AddMediatR(typeof(ServiceRegistration));
+        }
+    }
+}
