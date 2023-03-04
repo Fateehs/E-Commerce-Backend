86c753ce073defaef0697f3b25d9cf8ee2d09301
Fateeh <benzer1010@gmail.com>
2023-03-04 18:01:31 +0300

    Exception Class created, Methods added

---
+++ Core/ETradeAPI.Application/Exceptions/UserCreateFailedException.cs
@@ -0,0 +1,23 @@
+﻿using System.Runtime.Serialization;
+
+namespace ETradeAPI.Application.Exceptions
+{
+    public class UserCreateFailedException : Exception
+    {
+        public UserCreateFailedException() : base("An unexpected error was encountered while creating a user.")
+        {
+        }
+
+        public UserCreateFailedException(string? message) : base(message)
+        {
+        }
+
+        public UserCreateFailedException(string? message, Exception? innerException) : base(message, innerException)
+        {
+        }
+
+        protected UserCreateFailedException(SerializationInfo info, StreamingContext context) : base(info, context)
+        {
+        }
+    }
+}
