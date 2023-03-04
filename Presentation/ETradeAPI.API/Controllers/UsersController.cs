378273dffa6265c1b49159278c0062e0a1ba1fb0
Fateeh <benzer1010@gmail.com>
2023-03-04 18:04:44 +0300

    Controller created

---
+++ Presentation/ETradeAPI.API/Controllers/UsersController.cs
@@ -0,0 +1,22 @@
+﻿using ETradeAPI.Application.Features.Commands.AppUser.CreateUser;
+using MediatR;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ETradeAPI.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UsersController : Controller
+    {
+       readonly IMediator _mediator;
+
+        public UsersController(IMediator mediator)
+        {
+            _mediator = mediator;
+        }
+        public Task <IActionResult> Create(CreateUserCommandRequest createUserCommandRequest)
+        {
+            return Ok();
+        }
+    }
+}
