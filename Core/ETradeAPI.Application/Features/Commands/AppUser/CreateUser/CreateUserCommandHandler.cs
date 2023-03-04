92a274a7a0f53f861e3a11462ca88661f77e80b9
Fateeh <benzer1010@gmail.com>
2023-03-04 18:01:49 +0300

    CQRS Pattern implemented, Method created

---
+++ Core/ETradeAPI.Application/Features/Commands/AppUser/CreateUser/CreateUserCommandHandler.cs
@@ -0,0 +1,35 @@
+﻿using ETradeAPI.Application.Exceptions;
+using MediatR;
+using Microsoft.AspNetCore.Identity;
+
+namespace ETradeAPI.Application.Features.Commands.AppUser.CreateUser
+{
+    public class CreateUserCommandHandler : IRequestHandler<CreateUserCommandRequest, CreateUserCommandResponse>
+    {
+        readonly UserManager<Domain.Entities.Identity.AppUser> _userManager;
+
+        public CreateUserCommandHandler(UserManager<Domain.Entities.Identity.AppUser> userManager)
+        {
+            _userManager = userManager;
+        }
+
+        public async Task<CreateUserCommandResponse> Handle(CreateUserCommandRequest request, CancellationToken cancellationToken)
+        {
+            IdentityResult identityResult = await _userManager.CreateAsync(new()
+            {
+                UserName = request.Username,
+                Email = request.Email,
+                NameSurname = request.NameSurname,
+            }, request.Password);
+
+            if (identityResult.Succeeded)
+                return new()
+                {
+                    Succeeded = true,
+                    Message = "User created successfully."
+                };
+            throw new UserCreateFailedException();
+
+        }
+    }
+}
---
+++ Core/ETradeAPI.Application/Features/Commands/AppUser/CreateUser/CreateUserCommandResponse.cs
@@ -0,0 +1,8 @@
+﻿namespace ETradeAPI.Application.Features.Commands.AppUser.CreateUser
+{
+    public class CreateUserCommandResponse
+    {
+        public bool Succeeded { get; set; }
+        public string Message { get; set; }
+    }
+}
