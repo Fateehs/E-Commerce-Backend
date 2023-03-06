25090aa0fd65e83200d2483c4513367d5dd8d32d
Fateeh <benzer1010@gmail.com>
2023-03-06 17:41:43 +0300

    Concrete of IToken created, Method added

---
+++ Infrastructure/ETradeAPI.Infrastructure/Services/Token/TokenHandler.cs
@@ -0,0 +1,42 @@
+﻿using ETradeAPI.Application.Abstractions.Token;
+using Microsoft.Extensions.Configuration;
+using Microsoft.IdentityModel.Tokens;
+using System.IdentityModel.Tokens.Jwt;
+using System.Text;
+
+namespace ETradeAPI.Infrastructure.Services.Token
+{
+    public class TokenHandler : ITokenHandler
+    {
+        readonly IConfiguration _configuration;
+
+        public TokenHandler(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
+
+        public Application.DTOs.Token CreateAccessToken(int minute)
+        {
+            Application.DTOs.Token token = new();
+
+            SymmetricSecurityKey securityKey = new(Encoding.UTF8.GetBytes(_configuration["Token:SecurityKey"]));
+
+            SigningCredentials signingCredentials = new(securityKey, SecurityAlgorithms.HmacSha256);
+
+            token.Expiration = DateTime.UtcNow.AddMinutes(minute);
+
+            JwtSecurityToken jwtSecurityToken = new(
+                audience: _configuration["Token:Audience"],
+                issuer: _configuration["Token:Issuer"],
+                expires: token.Expiration,
+                notBefore: DateTime.UtcNow,
+                signingCredentials: signingCredentials
+                );
+
+            JwtSecurityTokenHandler jwtSecurityTokenHandler = new();
+            token.AccessToken = jwtSecurityTokenHandler.WriteToken(jwtSecurityToken);
+
+            return token;
+        }
+    }
+}
