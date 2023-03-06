b34e75b145c82510a8c2724b6bcdecfdf6cfe8aa
Fateeh <benzer1010@gmail.com>
2023-03-06 17:39:23 +0300

    Exception Class created, Exception added

---
+++ Core/ETradeAPI.Application/Exceptions/NotFoundUserException.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ETradeAPI.Application.Exceptions
+{
+    public class NotFoundUserException : Exception
+    {
+        public NotFoundUserException() : base("Username or password incorrect")
+        {
+            
+        }
+
+        public NotFoundUserException(string? message) : base(message)
+        {
+        }
+
+        public NotFoundUserException(string? message, Exception? innerException) : base(message, innerException)
+        {
+        }
+    }
+}
