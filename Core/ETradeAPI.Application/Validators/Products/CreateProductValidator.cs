e398db6bddc01352fb3e99a64bb4833148484786
Fateeh <benzer1010@gmail.com>
2023-03-06 17:40:34 +0300

    Validator modified, Unused Rules removed

--- Core/ETradeAPI.Application/Validators/Products/CreateProductValidator.cs
+++ Core/ETradeAPI.Application/Validators/Products/CreateProductValidator.cs
@@ -3,18 +3,18 @@ using FluentValidation;
 
 namespace ETradeAPI.Application.Validators.Products
 {
-    public class CreateProductValidator : AbstractValidator<VM_Create_Product>
+    public class CreateProductValidator 
     {
-        public CreateProductValidator()
-        {
-            RuleFor(p => p.Name).NotEmpty().NotNull().WithMessage("Please fill the product name.")
-                .MaximumLength(150).MinimumLength(2).WithMessage("The name of the product can be between 1 and 150 characters.");
+    //    public CreateProductValidator()
+    //    {
+    //        RuleFor(p => p.Name).NotEmpty().NotNull().WithMessage("Please fill the product name.")
+    //            .MaximumLength(150).MinimumLength(2).WithMessage("The name of the product can be between 1 and 150 characters.");
 
-            RuleFor(p => p.Price).NotEmpty().NotNull().WithMessage("Price cannot be blank.")
-                .Must(p => p >= 0).WithMessage("The price of the procut can be minumum 0.");
+    //        RuleFor(p => p.Price).NotEmpty().NotNull().WithMessage("Price cannot be blank.")
+    //            .Must(p => p >= 0).WithMessage("The price of the procut can be minumum 0.");
 
-            RuleFor(p => p.Stock).NotEmpty().NotNull().WithMessage("Please fill the stock amount")
-                .Must(s => s >= 0).WithMessage("The stock amount of the product can be minimum 0.");
-        }
+    //        RuleFor(p => p.Stock).NotEmpty().NotNull().WithMessage("Please fill the stock amount")
+    //            .Must(s => s >= 0).WithMessage("The stock amount of the product can be minimum 0.");
+    //    }
     }
 }
