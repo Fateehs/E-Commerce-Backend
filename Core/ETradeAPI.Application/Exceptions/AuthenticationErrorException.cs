16219fbf6fb8e7f0d94f0d8aa8b9544b88c89d47
Fateeh <benzer1010@gmail.com>
2023-03-08 20:57:30 +0300

    Exception created, Exception added

---
+++ Core/ETradeAPI.Application/Exceptions/AuthenticationErrorException.cs
@@ -0,0 +1,17 @@
+﻿namespace ETradeAPI.Application.Exceptions
+{
+    public class AuthenticationErrorException : Exception
+    {
+        public AuthenticationErrorException() : base("Username or password is wrong")
+        {
+        }
+
+        public AuthenticationErrorException(string? message) : base(message)
+        {
+        }
+
+        public AuthenticationErrorException(string? message, Exception? innerException) : base(message, innerException)
+        {
+        }
+    }
+}
