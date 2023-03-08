5a1f5263521cf990a220ed2290c7b98102380514
Fateeh <benzer1010@gmail.com>
2023-03-08 20:58:16 +0300

    Response modified, Property added

--- Core/ETradeAPI.Application/Features/Commands/AppUser/LoginUser/LoginUserCommandResponse.cs
+++ Core/ETradeAPI.Application/Features/Commands/AppUser/LoginUser/LoginUserCommandResponse.cs
@@ -1,6 +1,19 @@
-﻿namespace ETradeAPI.Application.Features.Commands.AppUser.LoginUser
+﻿using ETradeAPI.Application.DTOs;
+
+namespace ETradeAPI.Application.Features.Commands.AppUser.LoginUser
 {
     public class LoginUserCommandResponse
     {
+
+    }
+
+    public class LoginUserSuccessCommandResponse : LoginUserCommandResponse
+    {
+        public Token Token { get; set; }
+    }
+
+    public class LoginUserErrorCommandResponse : LoginUserCommandResponse
+    {
+        public string Message { get; set; }
     }
 }
