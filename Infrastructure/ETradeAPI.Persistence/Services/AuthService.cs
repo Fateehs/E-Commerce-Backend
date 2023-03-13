478f1e41a7fc2337b363a2a5eb5e992fe3c06820
Fateeh <benzer1010@gmail.com>
2023-03-13 06:06:04 +0300

    Concrete of Services created, Refactoring

---
+++ Infrastructure/ETradeAPI.Persistence/Services/AuthService.cs
@@ -0,0 +1,46 @@
+﻿using ETradeAPI.Application.Abstractions.Services;
+using ETradeAPI.Application.Abstractions.Token;
+using ETradeAPI.Application.DTOs;
+using ETradeAPI.Application.Exceptions;
+using ETradeAPI.Application.Features.Commands.AppUser.LoginUser;
+using ETradeAPI.Domain.Entities.Identity;
+using MediatR;
+using Microsoft.AspNetCore.Identity;
+
+namespace ETradeAPI.Persistence.Services
+{
+    public class AuthService : IAuthService
+    {
+        readonly UserManager<Domain.Entities.Identity.AppUser> _userManager;
+        readonly SignInManager<Domain.Entities.Identity.AppUser> _signInManager;
+        readonly ITokenHandler _tokenHandler;
+
+        public AuthService(UserManager<AppUser> userManager,
+            SignInManager<AppUser> signInManager,
+            ITokenHandler tokenHandler)
+        {
+            _userManager = userManager;
+            _signInManager = signInManager;
+            _tokenHandler = tokenHandler;
+        }
+
+        public async Task<Token> LoginAsync(string usernameOrEmail, string password, int accessTokenLifeTime)
+        {
+            Domain.Entities.Identity.AppUser user = await _userManager.FindByNameAsync(usernameOrEmail);
+            if (user == null)
+                user = await _userManager.FindByNameAsync(usernameOrEmail);
+
+            if (user == null)
+                throw new NotFoundUserException();
+            SignInResult result = await _signInManager.CheckPasswordSignInAsync(user, password, false);
+
+            if (result.Succeeded)
+            {
+                Token token = _tokenHandler.CreateAccessToken(accessTokenLifeTime);
+                return token;
+            }
+
+            throw new AuthenticationErrorException();
+        }
+    }
+}
---
+++ Infrastructure/ETradeAPI.Persistence/Services/UserService.cs
@@ -0,0 +1,40 @@
+﻿using ETradeAPI.Application.Abstractions.Services;
+using ETradeAPI.Application.DTOs.User;
+using ETradeAPI.Application.Features.Commands.AppUser.CreateUser;
+using ETradeAPI.Domain.Entities.Identity;
+using MediatR;
+using Microsoft.AspNetCore.Identity;
+
+namespace ETradeAPI.Persistence.Services
+{
+    public class UserService : IUserService
+    {
+        readonly UserManager<Domain.Entities.Identity.AppUser> _userManager;
+
+        public UserService(UserManager<AppUser> userManager)
+        {
+            _userManager = userManager;
+        }
+
+        public async Task<CreateUserResponse> CreateAsync(CreateUser model)
+        {
+            IdentityResult result = await _userManager.CreateAsync(new()
+            {
+                Id = Guid.NewGuid().ToString(),
+                UserName = model.Username,
+                Email = model.Email,
+                NameSurname = model.NameSurname,
+            }, model.Password);
+
+            CreateUserResponse response = new() { Succeeded = result.Succeeded };
+
+            if (result.Succeeded)
+                response.Message = "User created successfully.";
+            else
+                foreach (var error in result.Errors)
+                    response.Message += $"{error.Code} - {error.Description}\n";
+
+            return response;
+        }
+    }
+}
