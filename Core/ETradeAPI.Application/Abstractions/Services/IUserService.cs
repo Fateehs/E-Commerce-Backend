ee2edb8021b2f00abc22b9990ee6bcb6c406237f
Fateeh <benzer1010@gmail.com>
2023-03-15 22:00:24 +0300

    Interface modified, Method added

--- Core/ETradeAPI.Application/Abstractions/Services/IUserService.cs
+++ Core/ETradeAPI.Application/Abstractions/Services/IUserService.cs
@@ -1,10 +1,11 @@
 ﻿using ETradeAPI.Application.DTOs.User;
+using ETradeAPI.Domain.Entities.Identity;
 
 namespace ETradeAPI.Application.Abstractions.Services
 {
     public interface IUserService
     {
         Task<CreateUserResponse> CreateAsync(CreateUser model);
-        Task UpdateRefreshToken(string refreshToken, string userId, DateTime accessTokenDate, int refreshTokenLifeTime);
+        Task UpdateRefreshToken(string refreshToken, AppUser user, DateTime accessTokenDate, int refreshTokenLifeTime);
     }
 }
