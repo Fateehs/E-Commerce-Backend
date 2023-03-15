a451a60d2198fb923205bc8dbbe956f6c40155a0
Fateeh <benzer1010@gmail.com>
2023-03-15 22:02:16 +0300

    Controller modified, Action added

--- Presentation/ETradeAPI.API/Controllers/AuthController.cs
+++ Presentation/ETradeAPI.API/Controllers/AuthController.cs
@@ -1,4 +1,5 @@
 ﻿using ETradeAPI.Application.Features.Commands.AppUser.LoginUser;
+using ETradeAPI.Application.Features.Commands.AppUser.RefreshTokenLogin;
 using MediatR;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
@@ -20,6 +21,14 @@ namespace ETradeAPI.API.Controllers
         public async Task<IActionResult> Login(LoginUserCommandRequest loginUserCommandRequest)
         {
             LoginUserCommandResponse response = await _mediator.Send(loginUserCommandRequest);
+
+            return Ok(response);
+        }
+
+        [HttpPost("[action]")]
+        public async Task<IActionResult> RefreshTokenLogin([FromBody]RefreshTokenLoginCommandRequest refreshTokenLoginCommandRequest)
+        {
+            RefreshTokenLoginCommandResponse response = await _mediator.Send(refreshTokenLoginCommandRequest);
 
             return Ok(response);
         }
