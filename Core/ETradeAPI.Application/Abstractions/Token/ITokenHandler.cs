1af1440f21edc4910fbc71a953924c336fd8b101
Fateeh <benzer1010@gmail.com>
2023-03-16 14:32:03 +0300

    Interface modified, Method Parameter changed

--- Core/ETradeAPI.Application/Abstractions/Token/ITokenHandler.cs
+++ Core/ETradeAPI.Application/Abstractions/Token/ITokenHandler.cs
@@ -1,9 +1,10 @@
-﻿namespace ETradeAPI.Application.Abstractions.Token
+﻿using ETradeAPI.Domain.Entities.Identity;
+
+namespace ETradeAPI.Application.Abstractions.Token
 {
     public interface ITokenHandler
     {
-        DTOs.Token CreateAccessToken(int second);
-
+        DTOs.Token CreateAccessToken(int second, AppUser appUser);
         string CreateRefreshToken();
     }
 }
