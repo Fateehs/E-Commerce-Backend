dd36563770e346e7f693363cb870f7cc78dc4824
Fateeh <benzer1010@gmail.com>
2023-03-16 14:38:59 +0300

    Configuration Class created

---
+++ Presentation/ETradeAPI.API/Configurations/ColumnWriters/UsernameColumnWriter.cs
@@ -0,0 +1,19 @@
+﻿using NpgsqlTypes;
+using Serilog.Events;
+using Serilog.Sinks.PostgreSQL;
+
+namespace ETradeAPI.API.Configurations.ColumnWriters
+{
+    public class UsernameColumnWriter : ColumnWriterBase
+    {
+        public UsernameColumnWriter() : base(NpgsqlDbType.Varchar, 500)
+        {
+        }
+
+        public override object GetValue(LogEvent logEvent, IFormatProvider formatProvider = null)
+        {
+            var (username, value) = logEvent.Properties.FirstOrDefault(p => p.Key == "user_name");
+            return value?.ToString() ?? null;
+        }
+    }
+}
