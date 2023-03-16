06f095de44dfe8967e53804293e764ff3793b89e
Fateeh <benzer1010@gmail.com>
2023-03-16 14:38:10 +0300

    Class modified, Claim added

--- Infrastructure/ETradeAPI.Infrastructure/Services/Token/TokenHandler.cs
+++ Infrastructure/ETradeAPI.Infrastructure/Services/Token/TokenHandler.cs
@@ -1,7 +1,9 @@
 ﻿using ETradeAPI.Application.Abstractions.Token;
+using ETradeAPI.Domain.Entities.Identity;
 using Microsoft.Extensions.Configuration;
 using Microsoft.IdentityModel.Tokens;
 using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
 using System.Security.Cryptography;
 using System.Text;
 
@@ -16,7 +18,7 @@ namespace ETradeAPI.Infrastructure.Services.Token
             _configuration = configuration;
         }
 
-        public Application.DTOs.Token CreateAccessToken(int second)
+        public Application.DTOs.Token CreateAccessToken(int second, AppUser user)
         {
             Application.DTOs.Token token = new();
 
@@ -31,7 +33,8 @@ namespace ETradeAPI.Infrastructure.Services.Token
                 issuer: _configuration["Token:Issuer"],
                 expires: token.Expiration,
                 notBefore: DateTime.UtcNow,
-                signingCredentials: signingCredentials
+                signingCredentials: signingCredentials,
+                claims: new List<Claim> {new(ClaimTypes.Name, user.UserName) }
                 );
 
             JwtSecurityTokenHandler jwtSecurityTokenHandler = new();
