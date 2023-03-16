1287b8f2fd7a2600f8b7697d401a772f8d353078
Fateeh <benzer1010@gmail.com>
2023-03-16 14:37:39 +0300

    Handler Class modified, Logger added

--- Core/ETradeAPI.Application/Features/Queries/Product/GetAllProduct/GetAllProductQueryHandler.cs
+++ Core/ETradeAPI.Application/Features/Queries/Product/GetAllProduct/GetAllProductQueryHandler.cs
@@ -1,20 +1,25 @@
 ﻿using ETradeAPI.Application.Repositories;
 using ETradeAPI.Application.RequestParameters;
 using MediatR;
+using Microsoft.Extensions.Logging;
 
 namespace ETradeAPI.Application.Features.Queries.Product.GetAllProduct
 {
     public class GetAllProductQueryHandler : IRequestHandler<GetAllProductQueryRequest, GetAllProductQueryResponse>
     {
         readonly IProductReadRepository _productReadRepository;
-        public GetAllProductQueryHandler(IProductReadRepository productReadRepository)
+        readonly ILogger<GetAllProductQueryHandler> _logger;
+
+        public GetAllProductQueryHandler(IProductReadRepository productReadRepository,
+            ILogger<GetAllProductQueryHandler> logger)
         {
 
             _productReadRepository = productReadRepository;
-
+            _logger = logger;
         }
         public async Task<GetAllProductQueryResponse> Handle(GetAllProductQueryRequest request, CancellationToken cancellationToken)
         {
+            _logger.LogInformation("All Products Listed");
             var totalCount = _productReadRepository.GetAll(false).Count();
             var products = _productReadRepository.GetAll(false).Skip(request.Page * request.Size).Take(request.Size).Select(p => new
             {
