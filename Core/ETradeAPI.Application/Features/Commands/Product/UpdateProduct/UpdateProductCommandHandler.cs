0875db0b4d5bce1553188a7cdb2ac74c12f9b3e3
Fateeh <benzer1010@gmail.com>
2023-03-16 14:32:27 +0300

    Handler Class modified, Logger added

--- Core/ETradeAPI.Application/Features/Commands/Product/UpdateProduct/UpdateProductCommandHandler.cs
+++ Core/ETradeAPI.Application/Features/Commands/Product/UpdateProduct/UpdateProductCommandHandler.cs
@@ -1,5 +1,6 @@
 ﻿using ETradeAPI.Application.Repositories;
 using MediatR;
+using Microsoft.Extensions.Logging;
 using P = ETradeAPI.Domain.Entities;
 namespace ETradeAPI.Application.Features.Commands.Product.UpdateProduct
 {
@@ -7,11 +8,15 @@ namespace ETradeAPI.Application.Features.Commands.Product.UpdateProduct
     {
         readonly IProductReadRepository _productReadRepository;
         readonly IProductWriteRepository _productWriteRepository;
+        readonly ILogger<UpdateProductCommandHandler> _logger;
 
-        public UpdateProductCommandHandler(IProductReadRepository productReadRepository, IProductWriteRepository productWriteRepository)
+        public UpdateProductCommandHandler(IProductReadRepository productReadRepository,
+            IProductWriteRepository productWriteRepository,
+            ILogger<UpdateProductCommandHandler> logger)
         {
             _productReadRepository = productReadRepository;
             _productWriteRepository = productWriteRepository;
+            _logger = logger;
         }
 
         public async Task<UpdateProductCommandResponse> Handle(UpdateProductCommandRequest request, CancellationToken cancellationToken)
@@ -23,6 +28,7 @@ namespace ETradeAPI.Application.Features.Commands.Product.UpdateProduct
             product.Name = request.Name;
 
             await _productWriteRepository.SaveAsync();
+            _logger.LogInformation("Product Updated");
 
             return new();
         }
