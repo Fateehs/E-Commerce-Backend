905a0b737cd2142ab0d9c9e6eb08cbf72a08a4b7
Fateeh <benzer1010@gmail.com>
2023-03-17 07:37:38 +0300

    Concrete of Service created, Hub Context added

---
+++ Infrastructure/ETradeAPI.SignalR/HubServices/ProductHubService.cs
@@ -0,0 +1,22 @@
+﻿using ETradeAPI.Application.Abstractions.Hubs;
+using ETradeAPI.SignalR.Hubs;
+using Microsoft.AspNetCore.SignalR;
+
+namespace ETradeAPI.SignalR.HubServices
+{
+    public class ProductHubService : IProductHubService
+    {
+        readonly IHubContext<ProductHub> _hubContext;
+
+        public ProductHubService(IHubContext<ProductHub> hubContext)
+        {
+            _hubContext = hubContext;
+        }
+
+        public async Task ProductAddedMessageAsync(string message)
+        {
+            await _hubContext.Clients.All.SendAsync
+                (ReceiveFunctionNames.ProductAddedMessage, message);
+        }
+    }
+}
