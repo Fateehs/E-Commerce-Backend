09d8fc8253cb3b4ba5a5b01b60a2c99687897df2
Fateeh <benzer1010@gmail.com>
2023-03-17 07:38:39 +0300

    Hub Registration Class created,

---
+++ Infrastructure/ETradeAPI.SignalR/HubRegistration.cs
@@ -0,0 +1,13 @@
+﻿using ETradeAPI.SignalR.Hubs;
+using Microsoft.AspNetCore.Builder;
+
+namespace ETradeAPI.SignalR
+{
+    public static class HubRegistration
+    {
+        public static void MapHubs(this WebApplication webApplication)
+        {
+            webApplication.MapHub<ProductHub>("/products-hub");
+        }
+    }
+}
