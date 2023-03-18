90c21246ee6132f88f677fd5f30e0dcb8eefcac9
Fateeh <benzer1010@gmail.com>
2023-03-18 17:39:03 +0300

    IoC Service Container Class created

---
+++ Infrastructure/ETradeAPI.SignalR/ServiceRegistration.cs
@@ -0,0 +1,15 @@
+﻿using ETradeAPI.Application.Abstractions.Hubs;
+using ETradeAPI.SignalR.HubServices;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace ETradeAPI.SignalR
+{
+    public static class ServiceRegistration
+    {
+        public static void AddSignalRServices(this IServiceCollection services)
+        {
+            services.AddTransient<IProductHubService, ProductHubService>();
+            services.AddSignalR();
+        }
+    }
+}
