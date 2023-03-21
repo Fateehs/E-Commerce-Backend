1c069c5b0c1b2b41dafa08ee0f183ba41c03fbef
Fateeh <benzer1010@gmail.com>
2023-03-21 06:51:47 +0300

    View Models created, Properties added

---
+++ Core/ETradeAPI.Application/ViewModels/Products/VM_Create_Product.cs
@@ -0,0 +1,9 @@
+﻿namespace ETradeAPI.Application.ViewModels.Products
+{
+    public class VM_Create_Product
+    {
+        public string Name { get; set; }
+        public int Stock { get; set; }
+        public float Price { get; set; }
+    }
+}
