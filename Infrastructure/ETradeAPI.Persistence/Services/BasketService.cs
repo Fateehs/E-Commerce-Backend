af6d3dd8a4affde51bc5a35dec5a59689c39e1d3
Fateeh <benzer1010@gmail.com>
2023-03-21 06:54:44 +0300

    Concrete of Service created, Methods implemented

---
+++ Infrastructure/ETradeAPI.Persistence/Services/BasketService.cs
@@ -0,0 +1,29 @@
+﻿using ETradeAPI.Application.Abstractions.Services;
+using ETradeAPI.Application.ViewModels.Baskets;
+using ETradeAPI.Domain.Entities;
+
+namespace ETradeAPI.Persistence.Services
+{
+    public class BasketService : IBasketService
+    {
+        public Task AddItemToBasketAsync(VM_Create_BasketItem basketItem)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<List<BasketItem>> GetBasketItemAsync()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task RemoveBasketItemAsync(string basketItemId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task UpdateQuantityAsync(VM_Update_BasketItem basketItem)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
