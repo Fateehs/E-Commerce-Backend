b5ee55f85906b1aac50582ad7e60eb8afb8fc9aa
Fateeh <benzer1010@gmail.com>
2023-03-21 06:52:37 +0300

    Entity created, Properties added

---
+++ Core/ETradeAPI.Domain/Entities/Basket.cs
@@ -0,0 +1,14 @@
+﻿using ETradeAPI.Domain.Entities.Common;
+using ETradeAPI.Domain.Entities.Identity;
+
+namespace ETradeAPI.Domain.Entities
+{
+    public class Basket : BaseEntity
+    {
+        public string UserId { get; set; }
+
+        public AppUser User { get; set; } // Each basket can have "1" user
+        public Order Order { get; set; } // Each basket can have "1" order
+        public ICollection<BasketItem> BasketItems { get; set; } // Each basket can have "n" basket item
+    }
+}
---
+++ Core/ETradeAPI.Domain/Entities/BasketItem.cs
@@ -0,0 +1,14 @@
+﻿using ETradeAPI.Domain.Entities.Common;
+
+namespace ETradeAPI.Domain.Entities
+{
+    public class BasketItem : BaseEntity
+    {
+        public Guid BasketId { get; set; }
+        public Guid ProductId { get; set; }
+        public int Quantity { get; set; }
+
+        public Basket Basket { get; set; } // Each basket item can have "1" basket
+        public Product Product { get; set; } // Each basket item can have "1" product
+    }
+}
