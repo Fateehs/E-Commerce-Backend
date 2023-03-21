249f226c80e04df0c298ca8c5b2ce70e2c093b34
Fateeh <benzer1010@gmail.com>
2023-03-21 06:53:28 +0300

    Db Context Model modified, Tables added

--- Infrastructure/ETradeAPI.Persistence/Contexts/ETradeAPIDbContext.cs
+++ Infrastructure/ETradeAPI.Persistence/Contexts/ETradeAPIDbContext.cs
@@ -6,16 +6,31 @@ using Microsoft.EntityFrameworkCore;
 
 namespace ETradeAPI.Persistence.Contexts
 {
-    public class ETradeAPIDbContext : IdentityDbContext<AppUser,AppRole,string>
+    public class ETradeAPIDbContext : IdentityDbContext<AppUser, AppRole, string>
     {
         public ETradeAPIDbContext(DbContextOptions options) : base(options) { }
 
         public DbSet<Product> Products { get; set; }
         public DbSet<Order> Orders { get; set; }
         public DbSet<Customer> Customers { get; set; }
-        public DbSet<Domain.Entities.File> Files{ get; set; }
+        public DbSet<Domain.Entities.File> Files { get; set; }
         public DbSet<ProductImageFile> ProductImageFiles { get; set; }
         public DbSet<InvoiceFile> InvoiceFiles { get; set; }
+        public DbSet<Basket> Baskets { get; set; }
+        public DbSet<BasketItem> BasketItems { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            builder.Entity<Order>()
+                .HasKey(k => k.Id);
+
+            builder.Entity<Basket>()
+                .HasOne(k => k.Order)
+                .WithOne(o => o.Basket)
+                .HasForeignKey<Order>(k => k.Id);
+
+            base.OnModelCreating(builder);
+        }
 
         public override async Task<int> SaveChangesAsync(CancellationToken cancellationToken = default)
         {
