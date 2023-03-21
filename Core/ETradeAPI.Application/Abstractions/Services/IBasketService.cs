691e6fc9367bac08a39d19acf06d64ad988c3974
Fateeh <benzer1010@gmail.com>
2023-03-21 06:50:43 +0300

    Service created, Operations added

---
+++ Core/ETradeAPI.Application/Abstractions/Services/IBasketService.cs
@@ -0,0 +1,13 @@
+﻿using ETradeAPI.Application.ViewModels.Baskets;
+using ETradeAPI.Domain.Entities;
+
+namespace ETradeAPI.Application.Abstractions.Services
+{
+    public interface IBasketService
+    {
+        public Task<List<BasketItem>> GetBasketItemAsync();
+        public Task AddItemToBasketAsync(VM_Create_BasketItem basketItem);
+        public Task UpdateQuantityAsync(VM_Update_BasketItem basketItem);
+        public Task RemoveBasketItemAsync(string basketItemId);
+    }
+}
