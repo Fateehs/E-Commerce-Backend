1026f424119faaef086591788b1caab5fa582fc9
Fateeh <benzer1010@gmail.com>
2023-03-22 08:53:49 +0300

    Concrete of Service created, Methods implemented

--- Infrastructure/ETradeAPI.Persistence/Services/BasketService.cs
+++ Infrastructure/ETradeAPI.Persistence/Services/BasketService.cs
@@ -1,29 +1,132 @@
 ﻿using ETradeAPI.Application.Abstractions.Services;
+using ETradeAPI.Application.Repositories;
+using ETradeAPI.Application.Repositories.BasketItem;
 using ETradeAPI.Application.ViewModels.Baskets;
 using ETradeAPI.Domain.Entities;
+using ETradeAPI.Domain.Entities.Identity;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
 
 namespace ETradeAPI.Persistence.Services
 {
     public class BasketService : IBasketService
     {
-        public Task AddItemToBasketAsync(VM_Create_BasketItem basketItem)
+        readonly IHttpContextAccessor _httpContextAccessor;
+        readonly UserManager<AppUser> _userManager;
+        readonly IOrderReadRepository _orderReadRepository;
+        readonly IBasketWriteRepository _basketWriteRepository;
+        readonly IBasketReadRepository _basketReadRepository;
+        readonly IBasketItemWriteRepository _basketItemWriteRepository;
+        readonly IBasketItemReadRepository _basketItemReadRepository;
+        public BasketService(IHttpContextAccessor httpContextAccessor,
+            UserManager<AppUser> userManager,
+            IOrderReadRepository orderReadRepository,
+            IBasketWriteRepository basketWriteRepository,
+            IBasketItemWriteRepository basketItemWriteRepository,
+            IBasketItemReadRepository basketItemReadRepository,
+            IBasketReadRepository basketReadRepository)
         {
-            throw new NotImplementedException();
+            _httpContextAccessor = httpContextAccessor;
+            _userManager = userManager;
+            _orderReadRepository = orderReadRepository;
+            _basketWriteRepository = basketWriteRepository;
+            _basketItemWriteRepository = basketItemWriteRepository;
+            _basketItemReadRepository = basketItemReadRepository;
+            _basketReadRepository = basketReadRepository;
         }
 
-        public Task<List<BasketItem>> GetBasketItemAsync()
+        private async Task<Basket?> ContextUser()
         {
-            throw new NotImplementedException();
+            var username = _httpContextAccessor?.HttpContext?.User?.Identity?.Name;
+            if (!string.IsNullOrEmpty(username))
+            {
+                AppUser? user = await _userManager.Users
+                     .Include(u => u.Baskets)
+                     .FirstOrDefaultAsync(u => u.UserName == username);
+
+                var _basket = from basket in user.Baskets
+                              join order in _orderReadRepository.Table
+                              on basket.Id equals order.Id into BasketOrders
+                              from order in BasketOrders.DefaultIfEmpty()
+                              select new
+                              {
+                                  Basket = basket,
+                                  Order = order
+                              };
+
+
+                Basket? targetBasket = null;
+                if (_basket.Any(b => b.Order is null))
+                    targetBasket = _basket.FirstOrDefault(b => b.Order is null)?.Basket;
+                else
+                {
+                    targetBasket = new();
+                    user?.Baskets.Add(targetBasket);
+                }
+
+                await _basketWriteRepository.SaveAsync();
+                return targetBasket;
+            }
+            throw new Exception("An unexpected error was encountered");
         }
 
-        public Task RemoveBasketItemAsync(string basketItemId)
+        public async Task AddItemToBasketAsync(VM_Create_BasketItem basketItem)
         {
-            throw new NotImplementedException();
+            Basket? basket = await ContextUser();
+            if (basket != null)
+            {
+                BasketItem _basketItem = await _basketItemReadRepository.GetSingleAsync
+                       (bi => bi.BasketId == basket.Id && bi.ProductId == Guid.Parse(basketItem.ProductId));
+                if (_basketItem != null)
+                    _basketItem.Quantity++;
+                else
+                    await _basketItemWriteRepository.AddAsync(new()
+                    {
+                        BasketId = basket.Id,
+                        ProductId = Guid.Parse(basketItem.ProductId),
+                        Quantity = basketItem.Quantity
+                    });
+
+                await _basketItemWriteRepository.SaveAsync();
+            }
         }
 
-        public Task UpdateQuantityAsync(VM_Update_BasketItem basketItem)
+        public async Task<List<BasketItem>> GetBasketItemAsync()
         {
-            throw new NotImplementedException();
+            Basket? basket = await ContextUser();
+
+            Basket? result = await _basketReadRepository.Table
+                 .Include(b => b.BasketItems)
+                 .ThenInclude(bi => bi.Product)
+                 .FirstOrDefaultAsync(b => b.Id == basket.Id);
+
+            return result.BasketItems.ToList();
+        }
+
+        public async Task RemoveBasketItemAsync(string basketItemId)
+        {
+            BasketItem? basketItem = await _basketItemReadRepository.GetByIdAsync(basketItemId);
+
+            if (basketItem != null)
+            {
+                _basketItemWriteRepository.Remove(basketItem);
+
+                await _basketItemWriteRepository.SaveAsync();
+            }
+
+        }
+
+        public async Task UpdateQuantityAsync(VM_Update_BasketItem basketItem)
+        {
+            BasketItem? _basketItem = await _basketItemReadRepository.GetByIdAsync(basketItem.BasketItemId);
+
+            if (_basketItem != null)
+            {
+                _basketItem.Quantity = basketItem.Quantity;
+
+                await _basketItemWriteRepository.SaveAsync();
+            }
         }
     }
 }
