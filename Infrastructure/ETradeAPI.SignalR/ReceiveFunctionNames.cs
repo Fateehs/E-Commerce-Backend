26ffadf3ff26fe19ccf6471e2b3e160eeaa81822
Fateeh <benzer1010@gmail.com>
2023-03-24 21:05:22 +0300

    Const added

--- Infrastructure/ETradeAPI.SignalR/ReceiveFunctionNames.cs
+++ Infrastructure/ETradeAPI.SignalR/ReceiveFunctionNames.cs
@@ -9,5 +9,6 @@ namespace ETradeAPI.SignalR
     public static class ReceiveFunctionNames
     {
         public const string ProductAddedMessage = "receiveProductAddedMessage";
+        public const string OrderAddedMessage = "receiveOrderAddedMessage";
     }
 }
