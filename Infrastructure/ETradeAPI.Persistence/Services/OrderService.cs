cf3a70e02a4cd27c0cb66ca6e16776dfd0707604
Fateeh <benzer1010@gmail.com>
2023-03-24 21:03:36 +0300

    Concrete of Service created

---
+++ Infrastructure/ETradeAPI.Persistence/Services/OrderService.cs
@@ -0,0 +1,12 @@
+﻿using ETradeAPI.Application.Abstractions.Services;
+
+namespace ETradeAPI.Persistence.Services
+{
+    public class OrderService : IOrderService
+    {
+        public Task CreateOrder()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
