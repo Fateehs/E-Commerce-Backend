2719bee5153f47cdefc962c7c69b946bd46ffd7d
Fateeh <benzer1010@gmail.com>
2023-03-26 10:46:50 +0300

    Migration created

---
+++ Infrastructure/ETradeAPI.Persistence/Migrations/20230325133220_mig_4.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ETradeAPI.Persistence.Migrations
+{
+    /// <inheritdoc />
+    public partial class mig4 : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Orders_Customers_CustomerId",
+                table: "Orders");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Orders_CustomerId",
+                table: "Orders");
+
+            migrationBuilder.DropColumn(
+                name: "CustomerId",
+                table: "Orders");
+
+            migrationBuilder.AddColumn<Guid>(
+                name: "BasketId",
+                table: "Orders",
+                type: "uuid",
+                nullable: false,
+                defaultValue: new Guid("00000000-0000-0000-0000-000000000000"));
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "BasketId",
+                table: "Orders");
+
+            migrationBuilder.AddColumn<Guid>(
+                name: "CustomerId",
+                table: "Orders",
+                type: "uuid",
+                nullable: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Orders_CustomerId",
+                table: "Orders",
+                column: "CustomerId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Orders_Customers_CustomerId",
+                table: "Orders",
+                column: "CustomerId",
+                principalTable: "Customers",
+                principalColumn: "Id");
+        }
+    }
+}
