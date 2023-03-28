05a0c401927cf988c037c30d0ffb4ad0a9d17f75
Fateeh <benzer1010@gmail.com>
2023-03-28 13:01:59 +0300

    Controller modified, Operation added

--- Presentation/ETradeAPI.API/Controllers/OrdersController.cs
+++ Presentation/ETradeAPI.API/Controllers/OrdersController.cs
@@ -1,11 +1,28 @@
-﻿using Microsoft.AspNetCore.Http;
+﻿using ETradeAPI.Application.Features.Commands.Order.CreateOrder;
+using MediatR;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 
 namespace ETradeAPI.API.Controllers
 {
     [Route("api/[controller]")]
     [ApiController]
+    [Authorize(AuthenticationSchemes = "Admin")]
     public class OrdersController : ControllerBase
     {
+        readonly IMediator _mediator;
+
+        public OrdersController(IMediator mediator)
+        {
+            _mediator = mediator;
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> CreateOrder(CreateOrderCommandRequest createOrderCommandRequest)
+        {
+            CreateOrderCommandResponse response = await _mediator.Send(createOrderCommandRequest);
+
+            return Ok(response);
+        }
     }
 }
