551f21467c9013f4d670e1f57cf4f762c15b1934
Fateeh <benzer1010@gmail.com>
2023-03-28 13:00:30 +0300

    Command Class modified, Method added

--- Core/ETradeAPI.Application/Features/Commands/Order/CreateOrder/CreateOrderCommandHandler.cs
+++ Core/ETradeAPI.Application/Features/Commands/Order/CreateOrder/CreateOrderCommandHandler.cs
@@ -1,12 +1,36 @@
-﻿using MediatR;
+﻿using ETradeAPI.Application.Abstractions.Hubs;
+using ETradeAPI.Application.Abstractions.Services;
+using MediatR;
 
 namespace ETradeAPI.Application.Features.Commands.Order.CreateOrder
 {
     public class CreateOrderCommandHandler : IRequestHandler<CreateOrderCommandRequest, CreateOrderCommandResponse>
     {
-        public Task<CreateOrderCommandResponse> Handle(CreateOrderCommandRequest request, CancellationToken cancellationToken)
+        readonly IOrderService _orderService;
+        readonly IBasketService _basketService;
+        readonly IOrderHubService _orderHubService;
+
+        public CreateOrderCommandHandler(IOrderService orderService,
+            IBasketService basketService,
+            IOrderHubService orderHubService)
         {
-            throw new NotImplementedException();
+            _orderService = orderService;
+            _basketService = basketService;
+            _orderHubService = orderHubService;
+        }
+
+        public async Task<CreateOrderCommandResponse> Handle(CreateOrderCommandRequest request, CancellationToken cancellationToken)
+        {
+            await _orderService.CreateOrderAsync(new()
+            {
+                Address = request.Address,
+                Description = request.Description,
+                BasketId = _basketService.GetUserActiveBasket?.Id.ToString()
+            });
+
+            await _orderHubService.OrderAddedMessageAsync("A new order has been created!");
+
+            return new();
         }
     }
 }
--- Core/ETradeAPI.Application/Features/Commands/Order/CreateOrder/CreateOrderCommandRequest.cs
+++ Core/ETradeAPI.Application/Features/Commands/Order/CreateOrder/CreateOrderCommandRequest.cs
@@ -4,5 +4,7 @@ namespace ETradeAPI.Application.Features.Commands.Order.CreateOrder
 {
     public class CreateOrderCommandRequest : IRequest<CreateOrderCommandResponse>
     {
+        public string Description { get; set; }
+        public string Address { get; set; }
     }
 }
