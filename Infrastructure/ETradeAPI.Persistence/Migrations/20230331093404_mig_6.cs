6208110b02e47078e9697a4ec33a10af6b67a25b
Fateeh <benzer1010@gmail.com>
2023-03-31 15:28:35 +0300

    Migration created

---
+++ Infrastructure/ETradeAPI.Persistence/Migrations/20230331093404_mig_6.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ETradeAPI.Persistence.Migrations
+{
+    /// <inheritdoc />
+    public partial class mig6 : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "OrderCode",
+                table: "Orders",
+                type: "text",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Orders_OrderCode",
+                table: "Orders",
+                column: "OrderCode",
+                unique: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropIndex(
+                name: "IX_Orders_OrderCode",
+                table: "Orders");
+
+            migrationBuilder.DropColumn(
+                name: "OrderCode",
+                table: "Orders");
+        }
+    }
+}
