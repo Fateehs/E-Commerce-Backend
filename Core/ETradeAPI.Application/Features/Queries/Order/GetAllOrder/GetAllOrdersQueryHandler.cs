c51f42b2779611706b1f5b0bc7596f743f5beced
Fateeh <benzer1010@gmail.com>
2023-03-31 15:27:37 +0300

    CQRS Pattern implemented, Operations added

---
+++ Core/ETradeAPI.Application/Features/Queries/Order/GetAllOrder/GetAllOrdersQueryHandler.cs
@@ -0,0 +1,26 @@
+﻿using ETradeAPI.Application.Abstractions.Services;
+using MediatR;
+
+namespace ETradeAPI.Application.Features.Queries.Order.GetAllOrder
+{
+    public class GetAllOrdersQueryHandler : IRequestHandler<GetAllOrdersQueryRequest, GetAllOrdersQueryResponse>
+    {
+        readonly IOrderService _orderService;
+
+        public GetAllOrdersQueryHandler(IOrderService orderService)
+        {
+            _orderService = orderService;
+        }
+
+        public async Task<GetAllOrdersQueryResponse> Handle(GetAllOrdersQueryRequest request, CancellationToken cancellationToken)
+        {
+            var data = await _orderService.GetAllOrdersAsync(request.Page, request.Size);
+
+            return new()
+            {
+                TotalOrderCount = data.TotalOrderCount,
+                Orders = data.Orders
+            };
+        }
+    }
+}
---
+++ Core/ETradeAPI.Application/Features/Queries/Order/GetAllOrder/GetAllOrdersQueryResponse.cs
@@ -0,0 +1,8 @@
+﻿namespace ETradeAPI.Application.Features.Queries.Order.GetAllOrder
+{
+    public class GetAllOrdersQueryResponse
+    {
+        public int TotalOrderCount { get; set; }
+        public object Orders { get; set; }
+    }
+}
