f2f2c42a9aad2be7031d5f01b2371e12ce1aa009
Fateeh <benzer1010@gmail.com>
2023-04-05 16:44:58 +0300

    CQRS Pattern implemented, Methods added

---
+++ Core/ETradeAPI.Application/Features/Queries/Order/GetOrderById/GetOrderByIdQueryRequest.cs
@@ -0,0 +1,9 @@
+﻿using MediatR;
+
+namespace ETradeAPI.Application.Features.Queries.Order.GetOrderById
+{
+    public class GetOrderByIdQueryRequest : IRequest<GetOrderByIdQueryResponse>
+    {
+        public string Id { get; set; }
+    }
+} 
