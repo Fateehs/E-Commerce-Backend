f3b8afe7d7ea10d441cd60b1db36e0d5feb2b80f
Fateeh <benzer1010@gmail.com>
2023-04-13 15:52:15 +0300

    Abstract of Service created, Signatures created

---
+++ Core/ETradeAPI.Application/Abstractions/Services/IMailService.cs
@@ -0,0 +1,8 @@
+﻿namespace ETradeAPI.Application.Abstractions.Services
+{
+    public interface IMailService
+    {
+        Task SendMessageAsync(string to, string subject, string body, bool isBodyHtml = true);
+        Task SendMessageAsync(string[] tos, string subject, string body, bool isBodyHtml = true);
+    }
+}
