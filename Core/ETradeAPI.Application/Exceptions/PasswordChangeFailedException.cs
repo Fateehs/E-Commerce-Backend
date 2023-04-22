4565e95af285df040ca74bdbe594fdf38a8aff09
Fateeh <benzer1010@gmail.com>
2023-04-22 16:09:19 +0300

    Exception created

---
+++ Core/ETradeAPI.Application/Exceptions/PasswordChangeFailedException.cs
@@ -0,0 +1,18 @@
+﻿namespace ETradeAPI.Application.Exceptions
+{
+    public class PasswordChangeFailedException : Exception
+    {
+        public PasswordChangeFailedException() : base("An unexpected error was encountered while updating the password.")
+        {
+
+        }
+
+        public PasswordChangeFailedException(string? message) : base(message)
+        {
+        }
+
+        public PasswordChangeFailedException(string? message, Exception? innerException) : base(message, innerException)
+        {
+        }
+    }
+}
