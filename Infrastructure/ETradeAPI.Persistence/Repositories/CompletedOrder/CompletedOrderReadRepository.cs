5f09fbbb4e562ffb5acdaf711d48a338e83eea9d
Fateeh <benzer1010@gmail.com>
2023-04-23 10:04:46 +0300

    Concrete of Repository created

---
+++ Infrastructure/ETradeAPI.Persistence/Repositories/CompletedOrder/CompletedOrderReadRepository.cs
@@ -0,0 +1,13 @@
+﻿using ETradeAPI.Application.Repositories;
+using ETradeAPI.Domain.Entities;
+using ETradeAPI.Persistence.Contexts;
+
+namespace ETradeAPI.Persistence.Repositories
+{
+    public class CompletedOrderReadRepository : ReadRepository<CompletedOrder>, ICompletedOrderReadRepository
+    {
+        public CompletedOrderReadRepository(ETradeAPIDbContext context) : base(context)
+        {
+        }
+    }
+}
---
+++ Infrastructure/ETradeAPI.Persistence/Repositories/CompletedOrder/CompletedOrderWriteRepository.cs
@@ -0,0 +1,13 @@
+﻿using ETradeAPI.Application.Repositories;
+using ETradeAPI.Domain.Entities;
+using ETradeAPI.Persistence.Contexts;
+
+namespace ETradeAPI.Persistence.Repositories
+{
+    public class CompletedOrderWriteRepository : WriteRepository<CompletedOrder>, ICompletedOrderWriteRepository
+    {
+        public CompletedOrderWriteRepository(ETradeAPIDbContext context) : base(context)
+        {
+        }
+    }
+}
