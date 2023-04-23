135f70deee070762e9a5ccb4498d618b5605e6cd
Fateeh <benzer1010@gmail.com>
2023-04-23 10:06:04 +0300

    Entity created, Properties added and associated

---
+++ Core/ETradeAPI.Domain/Entities/CompletedOrder.cs
@@ -0,0 +1,11 @@
+﻿using ETradeAPI.Domain.Entities.Common;
+
+namespace ETradeAPI.Domain.Entities
+{
+    public class CompletedOrder : BaseEntity
+    {
+        public Guid OrderId { get; set; }
+
+        public Order Order { get; set; }
+    }
+}
