3285edc10e5695b25c79706688b56f4c11a2d7ca
Fateeh <benzer1010@gmail.com>
2023-04-23 10:03:27 +0300

    CQRS Pattern implemented, Operations added

---
+++ Core/ETradeAPI.Application/Features/Commands/Order/CompleteOrder/CompleteOrderCommandRequest.cs
@@ -0,0 +1,9 @@
+﻿using MediatR;
+
+namespace ETradeAPI.Application.Features.Commands.Order.CompleteOrder
+{
+    public class CompleteOrderCommandRequest : IRequest<CompleteOrderCommandResponse>
+    {
+        public string Id { get; set; }
+    }
+}
---
+++ Core/ETradeAPI.Application/Features/Commands/Order/CompleteOrder/CompleteOrderCommandResponse.cs
@@ -0,0 +1,6 @@
+﻿namespace ETradeAPI.Application.Features.Commands.Order.CompleteOrder
+{
+    public class CompleteOrderCommandResponse
+    {
+    }
+}
