70f46af4dc9c7d24f88bcd6a9f9c50f0ead25abb
Fateeh <benzer1010@gmail.com>
2023-04-23 10:06:24 +0300

    Db Context modified, Tables associated

--- Infrastructure/ETradeAPI.Persistence/Contexts/ETradeAPIDbContext.cs
+++ Infrastructure/ETradeAPI.Persistence/Contexts/ETradeAPIDbContext.cs
@@ -18,6 +18,7 @@ namespace ETradeAPI.Persistence.Contexts
         public DbSet<InvoiceFile> InvoiceFiles { get; set; }
         public DbSet<Basket> Baskets { get; set; }
         public DbSet<BasketItem> BasketItems { get; set; }
+        public DbSet<CompletedOrder> CompletedOrders { get; set; }
 
         protected override void OnModelCreating(ModelBuilder builder)
         {
@@ -32,6 +33,11 @@ namespace ETradeAPI.Persistence.Contexts
                 .HasOne(b => b.Order)
                 .WithOne(o => o.Basket)
                 .HasForeignKey<Order>(b => b.Id);
+
+            builder.Entity<Order>()
+                .HasOne(o => o.CompletedOrder)
+                .WithOne(c => c.Order)
+                .HasForeignKey<CompletedOrder>(c => c.OrderId);
 
             base.OnModelCreating(builder);
         }
