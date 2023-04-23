728f0073fba86cebad7075443dc5cd4065f0f83c
Fateeh <benzer1010@gmail.com>
2023-04-23 10:05:42 +0300

    Property associated with Table

--- Core/ETradeAPI.Domain/Entities/Order.cs
+++ Core/ETradeAPI.Domain/Entities/Order.cs
@@ -12,5 +12,6 @@ namespace ETradeAPI.Domain.Entities
         public Basket Basket { get; set; } // Each order can have "1" basket
         //public ICollection<Product> Products { get; set; }
         //public Customer Customer { get; set; }
+        public CompletedOrder CompletedOrder { get; set; }
     }
 }
