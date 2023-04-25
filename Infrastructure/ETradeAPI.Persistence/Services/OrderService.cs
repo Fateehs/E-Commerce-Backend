3e0d0a98b6a5aaea4438c675e489bcfdc7108cbb
Fateeh <benzer1010@gmail.com>
2023-04-26 02:26:48 +0300

    Concrete of Service modified, Operations added

--- Infrastructure/ETradeAPI.Persistence/Services/OrderService.cs
+++ Infrastructure/ETradeAPI.Persistence/Services/OrderService.cs
@@ -2,6 +2,7 @@
 using ETradeAPI.Application.Abstractions.Services;
 using ETradeAPI.Application.DTOs.Order;
 using ETradeAPI.Application.Repositories;
+using ETradeAPI.Domain.Entities;
 using ETradeAPI.Persistence.Repositories;
 using Microsoft.EntityFrameworkCore;
 
@@ -11,12 +12,32 @@ namespace ETradeAPI.Persistence.Services
     {
         readonly IOrderWriteRepository _orderWriteRepository;
         readonly IOrderReadRepository _orderReadRepository;
+        readonly ICompletedOrderWriteRepository _completedOrderWriteRepository;
+        readonly ICompletedOrderReadRepository _completedOrderReadRepository;
 
         public OrderService(IOrderWriteRepository orderWriteRepository,
-            IOrderReadRepository orderReadRepository)
+            IOrderReadRepository orderReadRepository,
+            ICompletedOrderWriteRepository completedOrderWriteRepository,
+            ICompletedOrderReadRepository completedOrderReadRepository)
         {
             _orderWriteRepository = orderWriteRepository;
             _orderReadRepository = orderReadRepository;
+            _completedOrderWriteRepository = completedOrderWriteRepository;
+            _completedOrderReadRepository = completedOrderReadRepository;
+        }
+
+        public async Task CompleteOrderAsync(string id)
+        {
+            Order order = await _orderReadRepository.GetByIdAsync(id);
+
+            if (order != null)
+            {
+                await _completedOrderWriteRepository.AddAsync(new()
+                {
+                    OrderId = Guid.Parse(id)
+                });
+                await _completedOrderWriteRepository.SaveAsync();
+            }
         }
 
         public async Task CreateOrderAsync(CreateOrder createOrder)
@@ -46,41 +67,70 @@ namespace ETradeAPI.Persistence.Services
             var data = query.Skip(page * size).Take(size);
             /*.Take((page * size)..size);*/
 
+            var data2 = from order in data
+                        join completedOrder in _completedOrderReadRepository.Table
+                           on order.Id equals completedOrder.OrderId into co
+                        from _co in co.DefaultIfEmpty()
+                        select new
+                        {
+                            Id = order.Id,
+                            CreatedDate = order.CreatedDate,
+                            OrderCode = order.OrderCode,
+                            Basket = order.Basket,
+                            Completed = _co != null ? true : false
+                        };
+
             return new()
             {
                 TotalOrderCount = await query.CountAsync(),
-                Orders = await data.Select(o => new
+                Orders = await data2.Select(o => new
                 {
                     Id = o.Id,
                     CreatedDate = o.CreatedDate,
                     OrderCode = o.OrderCode,
                     TotalPrice = o.Basket.BasketItems.Sum(bi => bi.Product.Price * bi.Quantity),
-                    UserName = o.Basket.User.UserName
+                    UserName = o.Basket.User.UserName,
+                    o.Completed
                 }).ToListAsync()
             };
         }
 
         public async Task<SingleOrder> GetOrderByIdAsync(string id)
         {
-            var data = await _orderReadRepository.Table
-                .Include(o => o.Basket)
-                .ThenInclude(b => b.BasketItems)
-                .ThenInclude(bi => bi.Product)
-                .FirstOrDefaultAsync(o => o.Id == Guid.Parse(id));
+            var data = _orderReadRepository.Table
+                                 .Include(o => o.Basket)
+                                     .ThenInclude(b => b.BasketItems)
+                                         .ThenInclude(bi => bi.Product);
+
+            var data2 = await (from order in data
+                               join completedOrder in _completedOrderReadRepository.Table
+                                    on order.Id equals completedOrder.OrderId into co
+                               from _co in co.DefaultIfEmpty()
+                               select new
+                               {
+                                   Id = order.Id,
+                                   CreatedDate = order.CreatedDate,
+                                   OrderCode = order.OrderCode,
+                                   Basket = order.Basket,
+                                   Completed = _co != null ? true : false,
+                                   Address = order.Address,
+                                   Description = order.Description
+                               }).FirstOrDefaultAsync(o => o.Id == Guid.Parse(id));
 
             return new()
             {
-                Id = data.Id.ToString(),
-                BasketItems = data.Basket.BasketItems.Select(bi => new
+                Id = data2.Id.ToString(),
+                BasketItems = data2.Basket.BasketItems.Select(bi => new
                 {
                     bi.Product.Name,
                     bi.Product.Price,
                     bi.Quantity
                 }),
-                Address = data.Address,
-                CreatedDate = data.CreatedDate,
-                Description = data.Description,
-                OrderCode = data.OrderCode,
+                Address = data2.Address,
+                CreatedDate = data2.CreatedDate,
+                Description = data2.Description,
+                OrderCode = data2.OrderCode,
+                Completed = data2.Completed
             };
         }
     }
