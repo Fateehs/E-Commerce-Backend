58ef640f6a27c0dab1e6242792b06c619df0d3b1
Fateeh <benzer1010@gmail.com>
2023-04-26 03:45:44 +0300

    Handler modified, Operation changed

--- Core/ETradeAPI.Application/Features/Commands/Order/CompleteOrder/CompleteOrderCommandHandler.cs
+++ Core/ETradeAPI.Application/Features/Commands/Order/CompleteOrder/CompleteOrderCommandHandler.cs
@@ -1,4 +1,5 @@
 ﻿using ETradeAPI.Application.Abstractions.Services;
+using ETradeAPI.Application.DTOs.Order;
 using MediatR;
 
 namespace ETradeAPI.Application.Features.Commands.Order.CompleteOrder
@@ -6,15 +7,20 @@ namespace ETradeAPI.Application.Features.Commands.Order.CompleteOrder
     public class CompleteOrderCommandHandler : IRequestHandler<CompleteOrderCommandRequest, CompleteOrderCommandResponse>
     {
         readonly IOrderService _orderService;
+        readonly IMailService _mailService;
 
-        public CompleteOrderCommandHandler(IOrderService orderService)
+        public CompleteOrderCommandHandler(IOrderService orderService, IMailService mailService)
         {
             _orderService = orderService;
+            _mailService = mailService;
         }
 
         public async Task<CompleteOrderCommandResponse> Handle(CompleteOrderCommandRequest request, CancellationToken cancellationToken)
         {
-            await _orderService.CompleteOrderAsync(request.Id);
+            (bool succeeded, CompletedOrderDTO dto) = await _orderService.CompleteOrderAsync(request.Id);
+
+            if (succeeded)
+                await _mailService.SendCompletedOrderMailAsync(dto.EMail, dto.OrderCode, dto.OrderDate, dto.Username);
 
             return new();
         }
