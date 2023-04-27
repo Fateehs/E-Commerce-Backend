a9e01b86bdaeb497512eff6a0e77382bf98376f4
Fateeh <benzer1010@gmail.com>
2023-04-27 10:50:47 +0300

    Controller created, Operation added

---
+++ Presentation/ETradeAPI.API/Controllers/ApplicationServicesController.cs
@@ -0,0 +1,26 @@
+﻿using ETradeAPI.Application.Abstractions.Services;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ETradeAPI.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ApplicationServicesController : ControllerBase
+    {
+        readonly IApplicationService _applicationService;
+
+        public ApplicationServicesController(IApplicationService applicationService)
+        {
+            _applicationService = applicationService;
+        }
+
+        [HttpGet]
+        public IActionResult GetAuthorizeDefinitionEndpoints()
+        {
+            var datas = _applicationService.GetAuthorizeDefinitionEndpoints(typeof(Program));
+
+            return Ok(datas);
+        }
+    }
+}
