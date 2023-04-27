f19d9785fb232249f342f0d3d285ee2973fbc6c8
Fateeh <benzer1010@gmail.com>
2023-04-27 10:49:40 +0300

    Const created

---
+++ Core/ETradeAPI.Application/Consts/AuthorizeDefinitonConstants.cs
@@ -0,0 +1,9 @@
+﻿namespace ETradeAPI.Application.Consts
+{
+    static public class AuthorizeDefinitonConstants
+    {
+        public const string Baskets = "Baskets";
+        public const string Orders = "Orders";
+        public const string Products = "Products";
+    }
+}
