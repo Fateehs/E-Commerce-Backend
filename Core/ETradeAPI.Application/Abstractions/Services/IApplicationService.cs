ab61239a3446821498ba0568ed4d5102658861ae
Fateeh <benzer1010@gmail.com>
2023-04-27 10:49:33 +0300

    Abstract of Service created, Signature added

---
+++ Core/ETradeAPI.Application/Abstractions/Services/IApplicationService.cs
@@ -0,0 +1,9 @@
+﻿using ETradeAPI.Application.DTOs.Configuration;
+
+namespace ETradeAPI.Application.Abstractions.Services
+{
+    public interface IApplicationService
+    {
+        List<Menu> GetAuthorizeDefinitionEndpoints(Type type);
+    }
+}
