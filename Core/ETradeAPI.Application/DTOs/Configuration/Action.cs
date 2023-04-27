2e5675b245d5fc257572c42ddf6a16f720d36286
Fateeh <benzer1010@gmail.com>
2023-04-27 10:49:59 +0300

    DTO created, Properties added

---
+++ Core/ETradeAPI.Application/DTOs/Configuration/Action.cs
@@ -0,0 +1,12 @@
+﻿using ETradeAPI.Application.Enums;
+
+namespace ETradeAPI.Application.DTOs.Configuration
+{
+    public class Action
+    {
+        public string ActionType { get; set; }
+        public string HttpType { get; set; }
+        public string Definition { get; set; }
+        public string Code { get; set; }
+    }
+}
