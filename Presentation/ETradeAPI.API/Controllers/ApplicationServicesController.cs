02bba3836e8d50ec41025b250fa4cca0b28d55b4
Fateeh <benzer1010@gmail.com>
2023-04-30 15:32:20 +0300

    Controller modified, Attributes added

--- Presentation/ETradeAPI.API/Controllers/ApplicationServicesController.cs
+++ Presentation/ETradeAPI.API/Controllers/ApplicationServicesController.cs
@@ -1,11 +1,16 @@
 ﻿using ETradeAPI.Application.Abstractions.Services;
+using ETradeAPI.Application.CustomAttributes;
+using ETradeAPI.Application.Enums;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Server.HttpSys;
 
 namespace ETradeAPI.API.Controllers
 {
     [Route("api/[controller]")]
     [ApiController]
+    [Authorize(AuthenticationSchemes = "Admin")]
     public class ApplicationServicesController : ControllerBase
     {
         readonly IApplicationService _applicationService;
@@ -16,6 +21,7 @@ namespace ETradeAPI.API.Controllers
         }
 
         [HttpGet]
+        [AuthorizeDefinition(ActionType = ActionType.Reading, Definition = "Get Authorize Definition Endpoints", Menu = "Application Services")]
         public IActionResult GetAuthorizeDefinitionEndpoints()
         {
             var datas = _applicationService.GetAuthorizeDefinitionEndpoints(typeof(Program));
