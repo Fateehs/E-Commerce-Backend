1f9bff94e406033dcc7779ff03a4f5b0bbb36cc9
Fateeh <benzer1010@gmail.com>
2023-05-03 11:42:30 +0300

    Entity created, Properties added

---
+++ Core/ETradeAPI.Domain/Entities/Endpoint.cs
@@ -0,0 +1,20 @@
+﻿using ETradeAPI.Domain.Entities.Common;
+using ETradeAPI.Domain.Entities.Identity;
+
+namespace ETicaretAPI.Domain.Entities
+{
+    public class Endpoint : BaseEntity
+    {
+        public Endpoint()
+        {
+            Roles = new HashSet<AppRole>();
+        }
+        public string ActionType { get; set; }
+        public string HttpType { get; set; }
+        public string Definition { get; set; }
+        public string Code { get; set; }
+
+        public Menu Menu { get; set; }
+        public ICollection<AppRole> Roles { get; set; }
+    }
+}
---
+++ Core/ETradeAPI.Domain/Entities/Menu.cs
@@ -0,0 +1,11 @@
+﻿using ETradeAPI.Domain.Entities.Common;
+
+namespace ETicaretAPI.Domain.Entities
+{
+    public class Menu : BaseEntity
+    {
+        public string Name { get; set; }
+
+        public ICollection<Endpoint> Endpoints { get; set; }
+    }
+}
