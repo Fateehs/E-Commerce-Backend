e338b873ce828207f435257d915c52370f685560
Fateeh <benzer1010@gmail.com>
2023-05-03 11:42:08 +0300

    Entity modified, Property added

--- Core/ETradeAPI.Domain/Entities/Identity/AppRole.cs
+++ Core/ETradeAPI.Domain/Entities/Identity/AppRole.cs
@@ -1,8 +1,10 @@
-﻿using Microsoft.AspNetCore.Identity;
+﻿using ETicaretAPI.Domain.Entities;
+using Microsoft.AspNetCore.Identity;
 
 namespace ETradeAPI.Domain.Entities.Identity
 {
     public class AppRole : IdentityRole<string>
     {
+        public ICollection<Endpoint> Endpoints { get; set; }
     }
 }
