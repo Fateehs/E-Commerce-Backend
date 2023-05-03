eac6121355fba2d2e953704b00c665e45307cdda
Fateeh <benzer1010@gmail.com>
2023-05-03 11:40:48 +0300

    Abstract of Service created, Signatures added

---
+++ Core/ETradeAPI.Application/Abstractions/Services/IAuthorizationEndpointService.cs
@@ -0,0 +1,8 @@
+﻿namespace ETicaretAPI.Application.Abstractions.Services
+{
+    public interface IAuthorizationEndpointService
+    {
+        public Task AssignRoleEndpointAsync(string[] roles, string menu, string code, Type type);
+        public Task<List<string>> GetRolesToEndpointAsync(string code, string menu);
+    }
+}
