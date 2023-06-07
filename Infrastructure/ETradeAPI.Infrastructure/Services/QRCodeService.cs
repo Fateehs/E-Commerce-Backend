ab5a64ac4597533328e0373f30f3c7e6c08f4f84
Fateeh <benzer1010@gmail.com>
2023-06-07 18:40:46 +0300

    Concrete of Service created, Signatures implemented

---
+++ Infrastructure/ETradeAPI.Infrastructure/Services/QRCodeService.cs
@@ -0,0 +1,17 @@
+﻿using ETradeAPI.Application.Abstractions.Services;
+using QRCoder;
+
+namespace ETradeAPI.Infrastructure.Services
+{
+    public class QRCodeService : IQRCodeService
+    {
+        public byte[] GenerateQrCode(string text)
+        {
+            QRCodeGenerator generator = new();
+            QRCodeData data = generator.CreateQrCode("qr generator test string", QRCodeGenerator.ECCLevel.Q);
+            PngByteQRCode qrCode = new(data);
+            byte[] byteGraphic = qrCode.GetGraphic(10, new byte[] { 84, 99, 71 }, new byte[] { 240, 240, 240 });
+            return byteGraphic;
+        }
+    }
+}
