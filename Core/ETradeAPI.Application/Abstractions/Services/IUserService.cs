33b9efaededeb0d555f2cd97cdde04d62bbf4036
Fateeh <benzer1010@gmail.com>
2023-06-07 18:34:42 +0300

    Service modified, Signatures added

--- Core/ETradeAPI.Application/Abstractions/Services/IUserService.cs
+++ Core/ETradeAPI.Application/Abstractions/Services/IUserService.cs
@@ -8,5 +8,10 @@ namespace ETradeAPI.Application.Abstractions.Services
         Task<CreateUserResponse> CreateAsync(CreateUser model);
         Task UpdateRefreshTokenAsync(string refreshToken, AppUser user, DateTime accessTokenDate, int refreshTokenLifeTime);
         Task UpdatePasswordAsync(string userId, string resetToken, string newPassword);
+        Task<List<ListUser>> GetAllUsersAsync(int page, int size);
+        int TotalUsersCount { get; }
+        Task AssignRoleToUserAsnyc(string userId, string[] roles);
+        Task<string[]> GetRolesToUserAsync(string userIdOrName);
+        Task<bool> HasRolePermissionToEndpointAsync(string name, string code);
     }
 }
