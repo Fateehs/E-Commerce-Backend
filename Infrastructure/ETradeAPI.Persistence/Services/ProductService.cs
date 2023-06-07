aed4ea32e560be88c6e7cc6cd047943819a2ac90
Fateeh <benzer1010@gmail.com>
2023-06-07 18:42:10 +0300

    Concrete of Service modified

---
+++ Infrastructure/ETradeAPI.Persistence/Services/ProductService.cs
@@ -0,0 +1,41 @@
+﻿using ETradeAPI.Application.Abstractions.Services;
+using ETradeAPI.Application.Repositories;
+using ETradeAPI.Domain.Entities;
+using System.Text.Json;
+
+namespace ETradeAPI.Persistence.Services
+{
+    public class ProductService : IProductService
+    {
+        readonly IProductReadRepository _productReadRepository;
+        readonly IQRCodeService _qrCodeService;
+
+        public ProductService(IProductReadRepository productReadRepository, IQRCodeService qrCodeService)
+        {
+            _productReadRepository = productReadRepository;
+            _qrCodeService = qrCodeService;
+        }
+
+        public async Task<byte[]> QrCodeToProductAsync(string productId)
+        {
+            Product product = await _productReadRepository.GetByIdAsync(productId);
+
+            if (product == null)
+            {
+                throw new Exception("Product not found");
+            }
+
+            var plainObject = new
+            {
+                product.Id,
+                product.Name,
+                product.Price,
+                product.Stock,
+                product.CreatedDate
+            };
+            string plainText = JsonSerializer.Serialize(plainObject);
+
+            return _qrCodeService.GenerateQrCode(plainText);
+        }
+    }
+}
--- Infrastructure/ETradeAPI.Persistence/Services/UserService.cs
+++ Infrastructure/ETradeAPI.Persistence/Services/UserService.cs
@@ -1,21 +1,25 @@
-﻿using ETradeAPI.Application.Abstractions.Services;
+﻿using ETicaretAPI.Application.Repositories;
+using ETicaretAPI.Domain.Entities;
+using ETradeAPI.Application.Abstractions.Services;
 using ETradeAPI.Application.DTOs.User;
 using ETradeAPI.Application.Exceptions;
-using ETradeAPI.Application.Features.Commands.AppUser.CreateUser;
 using ETradeAPI.Application.Helpers;
 using ETradeAPI.Domain.Entities.Identity;
-using MediatR;
 using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
 
-namespace ETradeAPI.Persistence.Services
+namespace ETicaretAPI.Persistence.Services
 {
     public class UserService : IUserService
     {
-        readonly UserManager<Domain.Entities.Identity.AppUser> _userManager;
+        readonly UserManager<AppUser> _userManager;
+        readonly IEndpointReadRepository _endpointReadRepository;
 
-        public UserService(UserManager<AppUser> userManager)
+        public UserService(UserManager<AppUser> userManager,
+            IEndpointReadRepository endpointReadRepository)
         {
             _userManager = userManager;
+            _endpointReadRepository = endpointReadRepository;
         }
 
         public async Task<CreateUserResponse> CreateAsync(CreateUser model)
@@ -31,14 +35,24 @@ namespace ETradeAPI.Persistence.Services
             CreateUserResponse response = new() { Succeeded = result.Succeeded };
 
             if (result.Succeeded)
-                response.Message = "User created successfully.";
+                response.Message = "Kullanıcı başarıyla oluşturulmuştur.";
             else
                 foreach (var error in result.Errors)
                     response.Message += $"{error.Code} - {error.Description}\n";
 
             return response;
         }
-
+        public async Task UpdateRefreshTokenAsync(string refreshToken, AppUser user, DateTime accessTokenDate, int addOnAccessTokenDate)
+        {
+            if (user != null)
+            {
+                user.RefreshToken = refreshToken;
+                user.RefreshTokenEndDate = accessTokenDate.AddSeconds(addOnAccessTokenDate);
+                await _userManager.UpdateAsync(user);
+            }
+            else
+                throw new NotFoundUserException();
+        }
         public async Task UpdatePasswordAsync(string userId, string resetToken, string newPassword)
         {
             AppUser user = await _userManager.FindByIdAsync(userId);
@@ -53,16 +67,93 @@ namespace ETradeAPI.Persistence.Services
             }
         }
 
-        public async Task UpdateRefreshTokenAsync(string refreshToken, AppUser user, DateTime accessTokenDate, int refreshTokenLifeTime)
+        public async Task<List<ListUser>> GetAllUsersAsync(int page, int size)
         {
+            var users = await _userManager.Users
+                  .Skip(page * size)
+                  .Take(size)
+                  .ToListAsync();
+
+            return users.Select(user => new ListUser
+            {
+                Id = user.Id,
+                Email = user.Email,
+                NameSurname = user.NameSurname,
+                TwoFactorEnabled = user.TwoFactorEnabled,
+                UserName = user.UserName
+
+            }).ToList();
+        }
+
+        public int TotalUsersCount => _userManager.Users.Count();
+
+        public async Task AssignRoleToUserAsnyc(string userId, string[] roles)
+        {
+            AppUser user = await _userManager.FindByIdAsync(userId);
             if (user != null)
             {
-                user.RefreshToken = refreshToken;
-                user.RefreshTokenEndDate = accessTokenDate.AddSeconds(refreshTokenLifeTime);
-                await _userManager.UpdateAsync(user);
+                var userRoles = await _userManager.GetRolesAsync(user);
+                await _userManager.RemoveFromRolesAsync(user, userRoles);
+
+                await _userManager.AddToRolesAsync(user, roles);
             }
-            else
-                throw new NotFoundUserException();
+        }
+        public async Task<string[]> GetRolesToUserAsync(string userIdOrName)
+        {
+            AppUser user = await _userManager.FindByIdAsync(userIdOrName);
+            if (user == null)
+                user = await _userManager.FindByNameAsync(userIdOrName);
+
+            if (user != null)
+            {
+                var userRoles = await _userManager.GetRolesAsync(user);
+                return userRoles.ToArray();
+            }
+            return new string[] { };
+        }
+
+        public async Task<bool> HasRolePermissionToEndpointAsync(string name, string code)
+        {
+            var userRoles = await GetRolesToUserAsync(name);
+
+            if (!userRoles.Any())
+                return false;
+
+            Endpoint? endpoint = await _endpointReadRepository.Table
+                     .Include(e => e.Roles)
+                     .FirstOrDefaultAsync(e => e.Code == code);
+
+            if (endpoint == null)
+                return false;
+
+            var hasRole = false;
+            var endpointRoles = endpoint.Roles.Select(r => r.Name);
+
+            //foreach (var userRole in userRoles)
+            //{
+            //    if (!hasRole)
+            //    {
+            //        foreach (var endpointRole in endpointRoles)
+            //            if (userRole == endpointRole)
+            //            {
+            //                hasRole = true;
+            //                break;
+            //            }
+            //    }
+            //    else
+            //        break;
+            //}
+
+            //return hasRole;
+
+            foreach (var userRole in userRoles)
+            {
+                foreach (var endpointRole in endpointRoles)
+                    if (userRole == endpointRole)
+                        return true;
+            }
+
+            return false;
         }
     }
 }
